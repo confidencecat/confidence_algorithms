ef563fa747bcca365946fe2ff06cc77c5d612e87
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-08-26 00:04:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+#define max(a, b) a > b ? a : b;
+
+int n, m = 0, a[1001], d[1001];
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	d[0] = 1;
+	
+	for (int i = 1; i < n; i++) {
+		for (int j = i - 1; j >= 0; j--) {
+			if (a[i] > a[j]) {
+				d[i] = max(d[j] + 1, d[i]);
+			}
+		}
+		if (d[i] == 0) {
+			d[i] = 1;
+		}
+	}
+	m = d[0];
+	for (int i = 1; i < n; i++) {
+		if (d[i] > m) m = d[i];
+	}
+	printf("%d\n", m);
+	return 0;
+}
