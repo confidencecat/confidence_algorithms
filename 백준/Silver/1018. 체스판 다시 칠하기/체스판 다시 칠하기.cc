d5528fd58e20a93934d9051ace30b5a9106407c6
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-08-27 23:01:38 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -4,46 +4,35 @@
 int main() {
 	//freopen("input.txt", "rt", stdin);
     char arr[50][51];
-    int a, b, minCnt = 3000;
+    int a, b;
 
-    scanf("%d%d", &a, &b);
+    scanf("%d %d", &a, &b);
     for (int i = 0; i < a; i++)
         scanf("%s", arr[i]);
 
-    int t1 = 0, t2 = 0;
-    for (int x = 0; x < a - 7; x++) {
-        for (int y = 0; y < b - 7; y++) {
-            for (int i = x; i < x + 8; i++) {
-                for (int j = y; j < y + 8; j++) {
-                    if (((i - x + j - y) % 2) == 0) {
-                        if (arr[i][j] != 'W')
-                            t1++;
+    int minChanges = 2500;
+
+    for (int x = 0; x <= a - 8; x++) {
+        for (int y = 0; y <= b - 8; y++) {
+            int startW = 0, startB = 0;
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if ((i + j) % 2 == 0) {
+                        if (arr[x + i][y + j] != 'W') startW++;
+                        if (arr[x + i][y + j] != 'B') startB++;
                     }
                     else {
-                        if (arr[i][j] != 'B')
-                            t1++;
+                        if (arr[x + i][y + j] != 'B') startW++;
+                        if (arr[x + i][y + j] != 'W') startB++;
                     }
                 }
             }
-            for (int i = x; i < x + 8; i++) {
-                for (int j = y; j < y + 8; j++) {
-                    if (((i - x + j - y) % 2) == 0) {
-                        if (arr[i][j] != 'B')
-                            t2++;
-                    }
-                    else {
-                        if (arr[i][j] != 'W')
-                            t2++;
-                    }
-                }
-            }
-            int res = t1 < t2 ? t1 : t2;
-            if (res < minCnt) minCnt = res;
-            t1 = 0;
-            t2 = 0;
+            int changes = startW < startB ? startW : startB;
+            if (changes < minChanges) minChanges = changes;
         }
     }
-    printf("%d", minCnt);
+    printf("%d", minChanges);
+
 
 	return 0;
 }
