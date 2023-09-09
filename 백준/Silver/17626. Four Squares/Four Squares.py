bf006ca32bb6c01fad27324710bfed527a448f98
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-09-09 10:49:57 +0900

    [Silver III] Title: Four Squares, Time: 60 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,20 @@
+import math
+ 
+def f(n):
+
+    if int(math.sqrt(n)) == math.sqrt(n):
+        return 1
+
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if int(math.sqrt(n - i**2)) == math.sqrt(n - i**2):
+            return 2
+
+    for i in range(1, int(math.sqrt(n)) + 1):
+        for j in range(1, int(math.sqrt(n - i**2)) + 1):
+            if int(math.sqrt(n - i**2 - j**2)) == math.sqrt(n - i**2 - j**2):
+                return 3
+    return 4
+ 
+ 
+n = int(input())
+print(f(n))
