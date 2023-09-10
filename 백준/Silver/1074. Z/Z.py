c099d9ece2ba03e604c141b211365cd4f0e9c656
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-09-10 23:22:57 +0900

    [Silver I] Title: Z, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -6,10 +6,10 @@ while n != 0:
 
     n -= 1
     if r < 2 ** n and c < 2 ** n:
-        ans += (2 ** n) * (2 ** n) * 0
+        ans += 0
 
     elif r < 2 ** n and c >= 2 ** n:
-        ans += (2 ** n) * (2 ** n) * 1
+        ans += (2 ** n) * (2 ** n)
         c -= (2 ** n)
 
     elif r >= 2 ** n and c < 2 ** n:
