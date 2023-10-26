3371fb3fb526ec733b5c1169ba312fefc38d9749
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-10-27 07:57:13 +0900

    [Gold IV] Title: 최단경로, Time: 148 ms, Memory: 8952 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits> // INT_MAX를 사용하기 위해
+using namespace std;
+
+int answer[20001];
+
+int main() {
+    //freopen("input.txt", "rt", stdin);
+    int v, e, s, nu, nv, nw;
+    scanf("%d %d %d", &v, &e, &s);
+    vector<pair<int, int>> a[20001];
+
+    for (int i = 1; i <= v; i++) {
+        answer[i] = INT_MAX;
+    }
+
+    for (int i = 0; i < e; i++) {
+        scanf("%d %d %d", &nu, &nv, &nw);
+        a[nu].push_back(make_pair(nv, nw));
+    }
+
+    answer[s] = 0;
+    priority_queue<pair<int, int>> q;
+    q.push(make_pair(0, s));
+
+    while (!q.empty()) {
+        int d = -q.top().first;
+        int c = q.top().second;
+        q.pop();
+        if (answer[c] < d) continue;
+        for (auto& p : a[c]) {
+            int next = p.first;
+            int nextd = d + p.second;
+
+            if (nextd < answer[next]) {
+                answer[next] = nextd;
+                q.push(make_pair(-nextd, next));
+            }
+        }
+    }
+
+    for (int i = 1; i <= v; i++) {
+        if (answer[i] == INT_MAX) printf("INF\n");
+        else printf("%d\n", answer[i]);
+    }
+    return 0;
+}
+
