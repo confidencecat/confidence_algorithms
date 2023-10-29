8e97215b9b9b7f3affce178a3c869d909d1d7305
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-10-29 23:08:17 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 1924 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,46 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
+#include <memory.h>
 using namespace std;
 
-int n, mx = 0;
-vector<pair<int, int>> a[10001];
+int n, mx = 0, endx;
+vector<pair<int, int>> a[10002];
 
-bool v[10001] = { false, };
+bool v[10002];
 
 void f(int s, int fv) {
+    v[s] = true;
     int fn = a[s].size();
-    if (fv > mx) mx = fv;
+    if (fv > mx) {
+        mx = fv;
+        endx = s;
+    }
     //printf("%d %d %d\n", s, fv, mx);
     for (int i = 0; i < fn; i++) {
         if (v[a[s][i].first] == true) continue;
 
-        v[a[s][i].first] = true;
-        v[s] = true;
         f(a[s][i].first, fv + a[s][i].second);
-        v[s] = false;
-        v[a[s][i].first] = false;
     }
 }
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int s, e, v;
+    int s, e, w;
     scanf("%d", &n);
-    if (n == 1) {
+    if(n == 1) {
         printf("0\n");
         return 0;
     }
     for (int i = 0; i < n; i++) {
-        scanf("%d %d %d", &s, &e, &v);
-        a[s].push_back({ e, v });
-        a[e].push_back({ s, v });
+        scanf("%d %d %d", &s, &e, &w);
+        a[s].push_back({ e, w });
+        a[e].push_back({ s, w });
     }
-
-    for (int i = 1; i <= n; i++) {
-        //printf("%d\n", i);
-        f(i, 0);
-    }
+    f(1, 0);
+    memset(v, false, sizeof(v));
+    f(endx, 0);
     printf("%d\n", mx);
     return 0;
 }
