89ae77d76ea6006d5a5e77c323419a82c5644a50
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-11-03 11:13:51 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -4,7 +4,7 @@
 int main() {
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i < n; i++) {
         for (int j = 0; j < i - 1; j++) {
             printf(" ");
         }
@@ -14,7 +14,6 @@ int main() {
         printf("\n");
     }
     for (int i = 0; i < n; i++) {
-        if (i == 0) continue;
         for (int j = 0; j < n - i - 1; j++) {
             printf(" ");
         }
