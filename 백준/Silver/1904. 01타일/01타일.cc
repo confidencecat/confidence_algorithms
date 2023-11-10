386c6662d4f90c40e4e779420a192aa9b930d182
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-11-10 21:04:41 +0900

    [Silver III] Title: 01타일, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -5,19 +5,15 @@
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int n;
+    int n, l1 = 0, l2 = 1, s = 1;
     scanf("%d", &n);
-    if (n == 1) printf("1\n");
-    else if (n == 2) printf("2\n");
-    else {
-        int l1 = 2, l2 = 1, s;
-        for (int i = 2; i < n; i++) {
-            s = (l2 + l1) % 15746;
-            l2 = l1;
-            l1 = s;
-        }
-        printf("%d\n", s);
+    for (int i = 0; i <= n; i++) {
+        s = l1 + l2;
+        if (s >= 15746) s -= 15746;
+        l2 = l1;
+        l1 = s;
     }
+    printf("%d\n", s);
     
 
     return 0;
