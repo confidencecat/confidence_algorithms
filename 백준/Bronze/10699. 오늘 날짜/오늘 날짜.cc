b9eb392b665748a3a3c196d587a64da2d404ca73
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-12-01 12:07:37 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<time.h>
+
+int main() {
+    struct tm* t;
+    time_t timer;
+    timer = time(NULL);
+    t = localtime(&timer);
+
+    printf("%d-%02d-%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+    return 0;
+}
