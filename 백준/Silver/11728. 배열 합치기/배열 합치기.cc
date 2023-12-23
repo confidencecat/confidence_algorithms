89fe79ec984a6fbbea663a81c2b07aac9cbe8715
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-12-23 17:38:53 +0900

    [Silver V] Title: 배열 합치기, Time: 816 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+int a[1000001], b[1000001];
+
+int main() {
+    //freopen("input.txt", "rt", stdin); 
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) scanf("%d ", &a[i]);
+    for (int i = 0; i < m; i++) scanf("%d ", &b[i]);
+
+    int i = 0, j = 0;
+    for (int k = 0; k < n + m; k++) {
+        if (i == n) {
+            printf("%d ", b[j++]);
+        }
+        else if (j == m) {
+            printf("%d ", a[i++]);
+        }
+        else if (a[i] < b[j]) {
+            printf("%d ", a[i++]);
+        }
+        else if (a[i] >= b[j]) {
+            printf("%d ", b[j++]);
+        }
+    }
+    return 0;
+}
