008abeeab55ed84a1b0b633c344cf6f26fa24d26
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-12-27 20:05:34 +0900

    [Gold I] Title: 구간 합 구하기, Time: 328 ms, Memory: 8944 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,60 +1,70 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-#include <vector>
-using namespace std;
+#include <stdlib.h>
+#include <math.h>
 
-long long int a[1000005] , tree[4000005];
-//vector<long long int> tree;
+typedef long long ll;
 
-long long int segment_tree(int s, int e, int node) {
-    if (s == e) return tree[node] = a[s];
-    int mid = (s + e) / 2;
-    return tree[node] = segment_tree(s, mid, node * 2) + segment_tree(mid + 1, e, node * 2 + 1);
+void update(ll* arr, ll* sqd, int sn, int i, ll z) {
+    ll tmp = arr[i];
+    arr[i] = z;
+    sqd[i / sn] += z - tmp;
 }
 
-long long int sum(int s, int e, int node, int l, int r) {
-    if (l > e || r < s) return 0;
-
-    if (l <= s && e <= r) return tree[node];
-
-    int mid = (s + e) / 2;
-
-    return sum(s, mid, node * 2, l, r) + sum(mid + 1, e, node * 2 + 1, l, r);
-}
-
-void update(int s, int e, int node, int index, long long int dif) {
-    if (index < s || index > e) return;
-    tree[node] += dif;
-    if (s == e) return;
-    int mid = (s + e) / 2;
-    update(s, mid, node * 2, index, dif);
-    update(mid + 1, e, node * 2 + 1, index, dif);
+ll query(ll* arr, ll* sqd, int sn, int n, int l, int r) {
+    ll ret = 0;
+    if (l / sn == r / sn) {
+        for (int i = l; i <= r; ++i) {
+            ret += arr[i];
+        }
+    }
+    else {
+        int j = l / sn * sn + sn < n ? l / sn * sn + sn : n;
+        for (int i = l; i < j; ++i) {
+            ret += arr[i];
+        }
+        for (int i = r / sn * sn; i <= r; ++i) {
+            ret += arr[i];
+        }
+        for (int i = l / sn + 1; i < r / sn; ++i) {
+            ret += sqd[i];
+        }
+    }
+    return ret;
 }
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int n, m, k;
+    int n, m, k, sn;
     scanf("%d %d %d", &n, &m, &k);
+    sn = (int)sqrt(n) + 1;
 
-    for (int i = 0; i < n; i++) {
-        scanf("%lld", &a[i]);
-    }
-    //tree.resize(n * 4 + 5);
-    segment_tree(0, n - 1, 1);
+    ll* arr = (ll*)malloc(n * sizeof(ll));
+    ll* sqd = (ll*)malloc(sn * sizeof(ll));
 
-    for (int i = 0; i < m + k; i++) {
-        int tf, ct;
-        long long cn;
-        scanf("%d %d %lld", &tf, &ct, &cn);
-        if (tf == 1) {
-            long long diff = cn - a[ct - 1];
-            update(0, n - 1, 1, ct - 1, diff);
-            a[ct - 1] = cn;
+    for (int i = 0; i < sn; ++i) {
+        sqd[i] = 0;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%lld", &arr[i]);
+        sqd[i / sn] += arr[i];
+    }
+
+    int T = m + k;
+    while (T--) {
+        ll a, b, c;
+        scanf("%lld %lld %lld", &a, &b, &c);
+        if (a == 1) {
+            update(arr, sqd, sn, b - 1, c);
         }
-        else if (tf == 2) {
-            printf("%lld\n", sum(0, n - 1, 1, ct - 1, cn - 1));
+        else {
+            printf("%lld\n", query(arr, sqd, sn, n, b - 1, c - 1));
         }
     }
-    
+
+    free(arr);
+    free(sqd);
+
     return 0;
 }
