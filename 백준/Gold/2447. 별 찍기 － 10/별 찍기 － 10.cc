44971a104d8ceb48fe77a0e449881b4a684b8b73
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-12-30 14:46:39 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 96 ms, Memory: 43160 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -1,19 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
-int n, a[6562][6561];
-
-void make(int x, int y) {
-    for (int i = x - 2; i <= x; i++) {
-        a[y - 2][i] = 1;
-        a[y][i] = 1;
-        if (i != x - 1) a[y - 1][i] = 1;
-    }
-}
+int n;
+char a[6562][6561];
 
 void f(int m, int x, int y) {
     if (m == 3) {
-        make(x, y);
+        for (int i = x - 2; i <= x; i++) {
+            a[y - 2][i] = '*';
+            a[y][i] = '*';
+            if (i != x - 1) a[y - 1][i] = '*';
+        }
     }
     else {
         for (int i = 3; i >= 1; i--) {
@@ -31,7 +28,7 @@ int main() {
     f(n, n, n);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            printf("%c", a[i][j] == 1 ? '*' : ' ');
+            printf("%c", a[i][j] ? a[i][j] : ' ');
         }
         printf("\n");
     }
