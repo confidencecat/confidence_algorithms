e7f9e64e899decc9b1ce8aa254d71ac9c4eb065f
confidencecat <89053337+confidencecat@users.noreply.github.com>
2023-12-31 22:59:20 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int n;
+
+void print_line(int m) {
+    for (int i = 0; i < m; i++) printf("____");
+}
+
+void f(int x) {
+    if (x == n) {
+        print_line(x);
+        printf("\"재귀함수가 뭔가요?\"\n");
+        print_line(x);
+        printf("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+        print_line(x);
+        printf("라고 답변하였지.\n");
+    }
+    else {
+        print_line(x);
+        printf("\"재귀함수가 뭔가요?\"\n");
+        print_line(x);
+        printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+        print_line(x);
+        printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+        print_line(x);
+        printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+        f(x + 1);
+        print_line(x);
+        printf("라고 답변하였지.\n");
+    }
+}
+
+int main() {
+    //freopen("input.txt", "rt", stdin);
+    
+    scanf("%d", &n);
+    printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+    f(0);
+    return 0;
+}
