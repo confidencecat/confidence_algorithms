1634a2ab1b8e7ed70772c7f342a928dfc162f6db
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-01-01 23:33:55 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -8,16 +8,17 @@ typedef struct {
 } Q;
 
 int main() {
+    //freopen("input.txt", "rt", stdin);
     int n, m, rx, ry, rd, ans = 0;
-    int map[52][52]; // 배열의 크기를 52x52로 조정
-    int md[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} }; // 상, 우, 하, 좌
+    int map[52][52];
+    int md[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
     queue<Q> q;
 
     scanf("%d %d", &n, &m);
     scanf("%d %d %d", &ry, &rx, &rd);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            scanf("%d", &map[i][j]); // 입력 포맷을 올바르게 수정
+            scanf("%d", &map[i][j]);
         }
     }
 
@@ -36,28 +37,28 @@ int main() {
 
         bool allCleaned = true;
         for (int i = 0; i < 4; i++) {
-            int nextD = (d + 3) % 4; // 회전하는 방향
+            int nextD = (d + 3) % 4;
             int nx = x + md[nextD][1];
             int ny = y + md[nextD][0];
 
             if (nx >= 0 && nx < m && ny >= 0 && ny < n && map[ny][nx] == 0) {
-                q.push({ nx, ny, nextD }); // 회전한 방향으로 전진
+                q.push({ nx, ny, nextD });
                 allCleaned = false;
                 break;
             }
-            d = nextD; // 다음 방향으로 회전
+            d = nextD;
         }
 
         if (allCleaned) {
-            int backD = (d + 2) % 4; // 반대 방향
+            int backD = (d + 2) % 4;
             int bx = x + md[backD][1];
             int by = y + md[backD][0];
 
             if (bx >= 0 && bx < m && by >= 0 && by < n && map[by][bx] != 1) {
-                q.push({ bx, by, d }); // 후진
+                q.push({ bx, by, d });
             }
             else {
-                break; // 더 이상 후진할 수 없으면 종료
+                break;
             }
         }
     }
