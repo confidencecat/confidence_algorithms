d69cc0235e56c9eb281b8f7542ba28e7e5b90a36
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-01-03 15:40:08 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if (x > 0 && y > 0) printf("1");
+	if (x > 0 && y < 0) printf("4");
+	if (x < 0 && y > 0) printf("2");
+	if (x < 0 && y < 0) printf("3");
+	return 0;
+}
