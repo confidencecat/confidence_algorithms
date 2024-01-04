8401f152f263dc3c2d746f637bfdc286e6de582f
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-01-04 22:08:47 +0900

    [Silver III] Title: 숫자 정사각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.cc
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.cc
@@ -3,12 +3,10 @@
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int n, m, ans = 0, max;
+    int n, m, ans = 0;
     char a[51][51];
     scanf("%d %d", &n, &m);
-    max = n > m ? m : n;
-
-    if (max == 1) {
+    if ((n > m ? m : n) == 1) {
         printf("1\n");
         return 0;
     }
@@ -19,11 +17,10 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            for (int k = 0; k < max; k++) {
+            for (int k = 0; k < (n > m ? m : n); k++) {
                 if (i + k >= n || j + k >= m) continue;
                 if (a[i][j] == a[i + k][j] && a[i][j] == a[i][j + k] && a[i][j] == a[i + k][j + k]) {
                     ans = ans > (k + 1) * (k + 1) ? ans : (k + 1) * (k + 1);
-                    //printf("%c %c %c %c\n", a[i][j], a[i][j + k], a[i + k][j], a[i + k][j + k]);
                 }
             }
         }
