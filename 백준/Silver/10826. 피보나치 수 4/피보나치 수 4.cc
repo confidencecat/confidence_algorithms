e88186278259cd20feb39b7a1cfaa6a2e77f6267
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-01-04 22:55:03 +0900

    [Silver V] Title: 피보나치 수 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -1,55 +1,52 @@
 #include <stdio.h>
 
-#define MAX 10000
+typedef long long ll;
 
-void add(int result[], int a[], int b[]) {
-    int carry = 0;
-    for (int i = 0; i < MAX; i++) {
-        int sum = a[i] + b[i] + carry;
-        result[i] = sum % 10;
-        carry = sum / 10;
-    }
-}
-
-void copy(int dest[], int src[]) {
-    for (int i = 0; i < MAX; i++) {
-        dest[i] = src[i];
-    }
-}
-
-void print(int num[]) {
-    int start = MAX;
-    while (start > 0 && num[start - 1] == 0) {
-        start--;
-    }
-    if (start == 0) {
-        printf("0\n");
-    } else {
-        for (int i = start - 1; i >= 0; i--) {
-            printf("%d", num[i]);
-        }
-        printf("\n");
-    }
-}
+ll arr[2][120];
 
 int main() {
     int n;
+    ll *a1, *a2, *tmp;
+    a1 = arr[0];
+    a2 = arr[1];
+    a1[0] = 0;
+    a2[0] = 1;
+
     scanf("%d", &n);
 
-    int fib1[MAX] = {0}, fib2[MAX] = {0}, result[MAX] = {0};
-
-    fib1[0] = fib2[0] = 1;
-    if (n == 0) printf("0\n");
-    else if (n <= 2) {
-        printf("1\n");
-    } else {
-        for (int i = 3; i <= n; i++) {
-            add(result, fib1, fib2);
-            copy(fib1, fib2);
-            copy(fib2, result);
+    ll p = 1000000000000000000LL;
+    for (int i = 0; i < n; i++) {
+        ll carry = 0;
+        for (int j = 0; (j < 120) && (a1[j] + carry != 0); j++) {
+            a2[j] += a1[j] + carry;
+            carry = a2[j] / p;
+            a2[j] %= p;
         }
-        print(result);
+
+        tmp = a1;
+        a1 = a2;
+        a2 = tmp;
     }
+
+    int flag = 0;
+    for (int i = 119; i >= 0; i--) {
+        if (flag == 0 && a1[i] == 0) {
+            if (i == 0) {
+                printf("0");
+            }
+            else {
+                continue;
+            }
+        }
+        else if (flag == 0) {
+            flag = 1;
+            printf("%lld", a1[i]);
+        }
+        else {
+            printf("%018lld", a1[i]);
+        }
+    }
+    printf("\n");
 
     return 0;
 }
