38cfe14cc3b5b868fc3c322cc2185b935873c343
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-01-07 19:53:22 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 4 ms, Memory: 1456 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -10,40 +10,38 @@ int main() {
     //freopen("input.txt", "rt", stdin);
     fgets(s, sizeof(s), stdin);
     int n = strlen(s);
-    if (s[n - 1] == '\n') s[n - 1] = '\0'; // 개행 문자 제거
-    n = strlen(s); // 개행 문자를 제거한 후의 길이 재계산
+    if (s[n - 1] == '\n') s[n - 1] = '\0';
+    n = strlen(s);
 
     vector<char> ns;
     bool tf = false;
     for (int i = 0; i < n; i++) {
         if (tf) {
-            // 태그 내부인 경우 즉시 출력
             printf("%c", s[i]);
             if (s[i] == '>') tf = false;
-        } else {
+        }
+        else {
             if (s[i] == '<') {
-                // 태그 시작 전 단어 뒤집어 출력
                 while (!ns.empty()) {
                     printf("%c", ns.back());
                     ns.pop_back();
                 }
                 tf = true;
                 printf("<");
-            } else if (s[i] == ' ') {
-                // 단어 뒤집어 출력 후 공백 출력
+            }
+            else if (s[i] == ' ') {
                 while (!ns.empty()) {
                     printf("%c", ns.back());
                     ns.pop_back();
                 }
                 printf(" ");
-            } else {
-                // 태그 및 공백이 아닌 경우 스택에 추가
+            }
+            else {
                 ns.push_back(s[i]);
             }
         }
     }
 
-    // 마지막 단어 뒤집어 출력
     while (!ns.empty()) {
         printf("%c", ns.back());
         ns.pop_back();
