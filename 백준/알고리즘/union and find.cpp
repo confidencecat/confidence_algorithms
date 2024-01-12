87695915917938f671d7af54e24b7a4506752c58
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-01-12 17:30:25 +0900

    Create union and find.cpp

---
+++ 백준/알고리즘/union and find.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int arr[10];
+
+int find(int a) {
+    if (arr[a] == a) return a;
+    return arr[a] = find(arr[a]);
+}
+
+void make_union(int a, int b) {
+    int find_a = find(a);
+    int find_b = find(b);
+    if (find_a != find_b) {
+        arr[b] = a;
+    }
+}
+
+int main() {
+    //freopen("input.txt", "rt", stdin);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; i++) arr[i] = i;
+
+    for (int i = 0; i < m; i++) {
+        int tf;
+        scanf("%d", &tf);
+        if (tf == 0) {
+            int a, b;
+            scanf("%d %d", &a, &b);
+            make_union(a, b);
+        }
+        else {
+            int a;
+            scanf("%d", &a);
+            printf("%d\n", find(a));
+        }
+    }
+    return 0;
+}v
