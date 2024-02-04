443aa3ee8631c5bfe8f7c49aa0ec8a99bc350d13
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-05 00:05:27 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.cc
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -1,19 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
 int main() {
-    int n, count = 9;
+    int n, count = 9, i = 0;
     long long dp[1023] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     scanf("%d", &n);
-
-    if (n > 1022) printf("-1\n");
-    else {
-        int i = 0;
-        while(count < n) {
-            for (int j = 0; j < dp[i] % 10; j++) dp[++count] = dp[i] * 10 + j;
-            i++;
-        }
-        printf("%lld\n", dp[n]);
+    if (n > 1022) {
+        printf("-1\n");
+        return 0;
     }
+    while (count < n) {
+        for (int j = 0; j < dp[i] % 10; j++) dp[++count] = dp[i] * 10 + j;
+        i++;
+    }
+    printf("%lld\n", dp[n]);
     return 0;
 }
