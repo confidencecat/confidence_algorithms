5cf2c2b56338b11e88ec4381f6e264b70fa70fb0
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-06 18:09:16 +0900

    [Gold IV] Title: 부분합, Time: 0 ms, Memory: 1632 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,16 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
-int n, s, a[100002], min_len, r = 1, l = 1;
+char Input[1 << 17];
+
+static inline char input()
+{
+    static int index = 1 << 17;
+    if (index == 1 << 17) {
+        Input[fread(Input, 1, 1 << 17, stdin)] = 0;
+        index = 0;
+    }
+    return Input[index++];
+}
+
+int read()
+{
+    char now = input();
+    register int val = 0;
+    while (now >= '0' && now <= '9') {
+        val = (val << 3) + (val << 1) + (now - '0');
+        now = input();
+    }
+    return val;
+}
+
+int n, s, a[100002], min_len;
 
 int main() {
-    scanf("%d %d", &n, &s);
+    //freopen("input.txt", "rt", stdin);
+    n = read();
+    s = read();
     min_len = n + 1;
     for (int i = 1; i <= n; i++) {
-        scanf("%d", &a[i]);
+        a[i] = read();
         a[i] += a[i - 1];
     }
-    while (l <= n && r <= n && l <= r) {
+    int r = 1, l = 1;
+
+    while (l <= n && r <= n) {
         int sum = a[r] - a[l - 1];
         if (sum < s) r++;
         else {
