2320267c46dddf23b4f06db76d496665b0e77008
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-09 14:40:35 +0900

    Update and rename 계단 자르기.cc to 계단 자르기 글자 수 최소.cc

--- 백준/Platinum/28287. 계단 자르기/계단 자르기.cc
+++
@@ -1,4 +0,0 @@
-#include<stdio.h>
-#define l long long
-#define fr(A,S,E) for(int A=S;A<=E;A++)
-l c[103]={1,},d[103],m,n;int main(){scanf("%d%lld",&n,&m);fr(i,1,n+2)fr(j,0,i-1)c[i]=(c[i]+(c[j]*c[i-j-1]%m))%m;fr(i,1,n){d[i]=(2*c[i+1]+c[i]-c[i+2]+m)%m;fr(j,1,i-1)d[i]=(d[i]+((2*(i-j)+1)*c[j]%m*c[i-j]%m))%m;fr(j,1,i)d[i]=(d[i]+(((c[j-1]*d[i-j])+(d[j-1]*c[i-j]))%m))%m;}fr(i,1,n)printf("%lld ",d[i]);}
