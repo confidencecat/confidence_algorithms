8f3470813da81660cd5e7c86863a32d93e9ab564
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-09 14:20:14 +0900

    [Platinum II] Title: 계단 자르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Platinum/28287. 계단 자르기/계단 자르기.cc
+++ 백준/Platinum/28287. 계단 자르기/계단 자르기.cc
@@ -1,13 +1,11 @@
 #include<stdio.h>
 #define l long long
 #define fr(A,S,E) for(int A=S;A<=E;A++)
-l c[103],d[103],m,n;
-int main() {
+l c[103]={1,},d[103],m,n;
+int main(){
     scanf("%d%lld",&n,&m);
-    c[0] = 1;
     fr(i,1,n+2)fr(j,0,i-1)c[i]=(c[i]+(c[j]*c[i-j-1]%m))%m;
-    d[1]=0;
-    fr(i,1,n) {
+    fr(i,1,n){
         d[i]=(2*c[i+1]+c[i]-c[i+2]+m)%m;
         fr(j,1,i-1)d[i]=(d[i]+((2*(i-j)+1)*c[j]%m*c[i-j]%m))%m;
         fr(j,1,i)d[i]=(d[i]+(((c[j-1]*d[i-j])+(d[j-1]*c[i-j]))%m))%m;
