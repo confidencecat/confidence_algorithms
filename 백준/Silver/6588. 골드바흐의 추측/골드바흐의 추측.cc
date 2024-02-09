23fb6d8d45c8ca9ecd022701f989d095ab7ea2b3
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-09 13:19:52 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 20 ms, Memory: 5392 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -3,6 +3,61 @@
 #include <vector>
 using namespace std;
 
+char rbuf[1 << 17];
+char wbuf[1 << 17];
+int idx, ridx, widx;
+
+inline char read() {
+    if (idx == ridx) {
+        ridx = fread(rbuf, 1, 1 << 17, stdin);
+        if (!ridx) return 0;
+        idx = 0;
+    }
+    return rbuf[idx++];
+}
+
+inline int readInt() {
+    int sum = 0;
+    char now = read();
+
+    while (now <= 32) now = read();
+    while (now >= 48) sum = sum * 10 + now - '0', now = read();
+
+    return sum;
+}
+
+inline int unitSize(int n) {
+    int ret = 1;
+    while (n >= 10) {
+        ret++;
+        n /= 10;
+    }
+    return ret;
+}
+
+void bflush() {
+    fwrite(wbuf, 1, widx, stdout);
+    widx = 0;
+}
+
+inline void writeInt(int n) {
+    int isz = unitSize(n);
+    if (isz + widx >= (1 << 17)) bflush();
+
+    int next = widx + isz;
+    while (isz--) {
+        wbuf[widx + isz] = n % 10 + '0';
+        n /= 10;
+    }
+    widx = next;
+}
+
+inline void write(char c) {
+    if (widx == (1 << 17)) bflush();
+    wbuf[widx++] = c;
+}
+
+
 int era[1000001], n = 0;
 
 int main() {
@@ -19,7 +74,7 @@ int main() {
 	}
 
 	while (1) {
-		scanf("%d", &n);
+        n = readInt();
 		if (n == 0) {
 			break;
 		}
@@ -27,10 +82,16 @@ int main() {
 		for (int i = 0; i < prime.size(); i++) {
 			if (prime[i] % 2 == 0 || (n - prime[i]) % 2 == 0) continue;
 			if (era[n - prime[i]] == 0) {
-				printf("%d = %d + %d\n", n, prime[i], n - prime[i]);
+                writeInt(n); write(' ');
+                write('='); write(' ');
+                writeInt(prime[i]);
+                write(' '); write('+');
+                write(' '); writeInt(n - prime[i]);
+                write('\n');
 				break;
 			}
 		}
 	}
+    bflush();
 	return 0;
 }
