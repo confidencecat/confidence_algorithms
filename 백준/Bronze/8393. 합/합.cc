98ac2981231c67ee84aa82a3adff85a4242aa64d
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-10 16:10:39 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int n, sum = 0;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) sum += i;
+	printf("%d\n", sum);
+	return 0;
+}
