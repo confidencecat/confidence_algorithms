e240c1a9ea2895bf9230948017b109ea139685f9
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-12 11:11:59 +0900

    [Silver IV] Title: 수 찾기, Time: 80 ms, Memory: 1892 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,28 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int narr[100000];
-int marr[100000];
+int arr[100000], n, m, ma;
 
-int f(int arr[], int key, int size) {
-    int f, r, p;
-    f = 0;
-    r = size - 1;
-
-    while (1) {
-        p = (f + r) / 2;
-        if (arr[p] == key) return 1;
-        if (arr[f] == key) return 1;
-        if (arr[r] == key) return 1;
-
-        if (arr[p] < key)
-            f = p + 1;
-        else
-            r = p - 1;
-
-        if (f >= r)
-            return 0;
+int f(int a) {
+    int start = 0;
+    int end = n - 1;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (arr[mid] == a) return 1;
+        else if (a < arr[mid]) end = mid - 1;
+        else start = mid + 1;
     }
+    return 0;
 }
 
 int cmp(const void* a, const void* b) {
@@ -31,23 +21,17 @@ int cmp(const void* a, const void* b) {
 }
 
 int main() {
-    int n, m;
-    int i;
-
     scanf("%d", &n);
-    for (i = 0; i < n; i++) {
-        scanf("%d", &narr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
+
+    qsort(arr, n, sizeof(int), cmp);
 
     scanf("%d", &m);
-    for (i = 0; i < m; i++) {
-        scanf("%d", &marr[i]);
-    }
-
-    qsort(narr, n, sizeof(int), cmp);
-
-    for (i = 0; i < m; i++) {
-        printf("%d\n", f(narr, marr[i], n));
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &ma);
+        printf("%d\n", f(ma));
     }
 
     return 0;
