5e652b737545890881d488ba09fbe9f39e97d731
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-22 22:58:59 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,43 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int n, m, a[21], c[21], answer = 0;
-
-void f(int x){
-    if (x == n) {
-        int ans = 1000001;
-        for (int i = 0; i < n; i++) {
-            if (c[i] == 1) {
-                if (ans == 1000001) {
-                    ans = 0;
-                }
-                ans += a[i];
-            }
-        }
-        if (ans == m && ans != 1000001) {
-            answer += 1;
-        }
+int n, s;
+int a[30];
+int sum;
+void f(int x, int fs) {
+    int i;
+    if (fs + a[x] == s) {
+        sum++;
     }
-    else {
-        c[x] = 0;
-        f(x + 1);
-        
-        c[x] = 1;
-        f(x + 1);
-    }
+    if (x == n) return;
+    f(x + 1, fs);
+    f(x + 1, fs + a[x]);
 
 }
 
-int main(){
+int main() {
     //freopen("input.txt", "rt", stdin);
-    scanf("%d %d", &n, &m);
-    for (int i = 0; i < n; i++) {
+    scanf("%d %d", &n, &s);
+    int i, j;
+    for (i = 1; i <= n; i++) {
         scanf("%d", &a[i]);
     }
-
-    f(0);
-
-    printf("%d\n", answer);
-
+    f(1, 0);
+    printf("%d", sum);
     return 0;
 }
