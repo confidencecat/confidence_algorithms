5feb83b3f862baf83025bcd835c49c824760625c
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-25 20:21:17 +0900

    Create lower_bound_function.cpp

---
+++ 알고리즘/lower_upper_bound/lower_bound_function.cpp
@@ -0,0 +1,20 @@
+int lower_bound(int list[], int len, int key) {
+	int start = 0, end = len - 1;
+	int ans = len - 1;
+
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		if (list[mid] > key) {
+			end = mid - 1;
+			ans = mid;
+		}
+		else if(list[mid] < key) {
+			start = mid + 1;
+		}
+		else {
+			ans = mid;
+			break;
+		}
+	}
+	return ans;
+}
