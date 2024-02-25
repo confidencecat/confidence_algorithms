5792f8e4d23d08512389418b33ca3a80b62914fb
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-02-25 20:22:37 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 232 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,32 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-#include <vector>
-using namespace std;
 
-int n, a, ln = 0;
+int n, a, ln = 0, l[1000001];
+
+int lower_bound(int key) {
+	int start = 0, end = ln - 1;
+	int ans = ln - 1;
+
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		if (l[mid] > key) {
+			end = mid - 1;
+			ans = mid;
+		}
+		else if (l[mid] < key) {
+			start = mid + 1;
+		}
+		else {
+			ans = mid;
+			break;
+		}
+	}
+	return ans;
+}
 
 int main() {
 	//freopen("input.txt", "rt", stdin);
-	vector<int> l;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &a);
 
-		int pos = lower_bound(l.begin(), l.end(), a) - l.begin();
-		//printf("%d\n", pos);
-		if (i == 0){
-			l.push_back(a);
-			ln++;
-		}
-		else if (pos >= ln) {
-			l.push_back(a);
-			ln++;
-		}
+		if (ln == 0) l[ln++] = a;
+		else if (l[ln - 1] < a) l[ln++] = a;
 		else {
+			int pos = lower_bound(a);
 			l[pos] = a;
 		}
 	}
-	//for (int& I : l) printf("%d ", I);
+	//for (int i = 0; i < ln; i++) printf("%d ", l[i]);
 	printf("%d\n", ln);
 	return 0;
 }
