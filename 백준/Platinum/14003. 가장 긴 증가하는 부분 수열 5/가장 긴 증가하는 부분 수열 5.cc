c240861be0c914cc16f0abaf3d343b43eae1d119
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-03-09 21:48:52 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 96 ms, Memory: 35116 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -2,7 +2,99 @@
 #include <stdio.h>
 #include <vector>
 using namespace std;
+#include <stdlib.h>
+#include <string.h>
 
+#define SZ (1 << 20)
+
+typedef struct {
+	char* read_buf;
+	int read_idx, next_idx;
+} INPUT;
+
+typedef struct {
+	char* write_buf;
+	int write_idx;
+} OUTPUT;
+
+INPUT input;
+OUTPUT output;
+
+void init_input() {
+	input.read_buf = (char*)malloc(SZ);
+	input.read_idx = input.next_idx = 0;
+}
+
+char scanfc() {
+	if (input.read_idx == input.next_idx) {
+		input.next_idx = fread(input.read_buf, 1, SZ, stdin);
+		if (!input.next_idx) return 0;
+		input.read_idx = 0;
+	}
+	return input.read_buf[input.read_idx++];
+}
+
+int is_blank(char c) { return c == ' ' || c == '\n' || c == '\r' || c == '\t'; }
+int scanfi() {
+	int result = 0, sign = 1;
+	char c = scanfc();
+	while (is_blank(c)) c = scanfc();
+	if (c == '-') { sign = -1; c = scanfc(); }
+	while (c >= '0' && c <= '9') { result = result * 10 + (c - '0'); c = scanfc(); }
+	return result * sign;
+}
+
+long long scanfl() {
+	long long result = 0;
+	int sign = 1;
+	char c = scanfc();
+	while (is_blank(c)) c = scanfc();
+	if (c == '-') { sign = -1; c = scanfc(); }
+	while (c >= '0' && c <= '9') { result = result * 10 + (c - '0'); c = scanfc(); }
+	return result * sign;
+}void cleanup_input() { free(input.read_buf); }
+
+void init_output() {
+	output.write_buf = (char*)malloc(SZ);
+	output.write_idx = 0;
+}
+
+void bflush() {
+	if (output.write_idx) {
+		fwrite(output.write_buf, 1, output.write_idx, stdout);
+		output.write_idx = 0;
+	}
+}void printfc(char c) {
+	if (output.write_idx == SZ) bflush();
+	output.write_buf[output.write_idx++] = c;
+}void printfi(int n) {
+	if (!n) { printfc('0'); return; }
+	if (n < 0) { printfc('-'); n = -n; }
+	char temp[12];
+	int i = 0;
+	while (n) { temp[i++] = n % 10 + '0'; n /= 10; }
+	while (i--) printfc(temp[i]);
+}
+
+void printfl(long long n) {
+	if (!n) { printfc('0'); return; }
+	if (n < 0) { printfc('-'); n = -n; }
+	char temp[20];
+	int i = 0;
+	while (n) { temp[i++] = n % 10 + '0'; n /= 10; }
+	while (i--) printfc(temp[i]);
+}
+
+void printfs(const char* str) {
+	while (*str) {
+		printfc(*str++);
+	}
+}
+
+void cleanup_output() {
+	bflush();
+	free(output.write_buf);
+}
 int n, ln = 0, ch[1000001];
 long long int a[1000001], l[1000001];
 vector<long long int> v;
@@ -29,10 +121,12 @@ int lower_bound( int key) {
 }
 
 int main() {
+	init_input();
+	init_output();
 	//freopen("input.txt", "rt", stdin);
-	scanf("%d", &n);
+	n = scanfi();
 	for (int i = 0; i < n; i++) {
-		scanf("%lld", &a[i]);
+		a[i] = scanfl();
 
 		if (ln == 0) {
 			l[ln++] = a[i];
@@ -49,7 +143,8 @@ int main() {
 			ch[i] = pos;
 		}
 	}
-	printf("%d\n", ln);
+	printfi(ln);
+	printfc('\n');
 	int cnt = ln - 1;
 	for (int i = n - 1; i >= 0; i--) {
 		if (ch[i] == cnt) {
@@ -58,8 +153,11 @@ int main() {
 		}
 	}
 	while (!v.empty()) {
-		printf("%lld ", v.back());
+		printfl(v.back());
+		printfc(' ');
 		v.pop_back();
 	}
+	cleanup_input();
+	cleanup_output();
 	return 0;
 }
