3044fcc5c32d897259a093fac04772f5f1ddbdf6
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-03-18 11:21:19 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int N, dp[1001], cnt = 0;
+
+bool f(int n) {
+    int l = n % 10;
+    int r = (n % 100) / 10;
+    int mi = r - l;
+    n /= 100;
+    while (n > 0) {
+        int no = n % 10;
+        if (no - r != mi) {
+            return false;
+        }
+        r = no;
+        n /= 10;
+    }
+    return true;
+}
+
+int main() {
+    //freopen("input.txt", "rt", stdin);
+    scanf("%d", &N);
+    //printf("%c", f(N) ? 'T' : 'F');
+
+    if (N < 10) {
+        printf("%d\n", N);
+    }
+    else {
+        cnt = 9;
+        for (int i = 10; i <= N; i++) {
+            if (f(i)) {
+                cnt++;
+            }
+        }
+        printf("%d\n", cnt);
+    }
+    
+    return 0;
+}
