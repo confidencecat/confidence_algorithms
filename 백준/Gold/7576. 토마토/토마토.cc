8eb43b30d9352492b6e120d68e2bb4d5d5665e3d
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-04-09 08:32:18 +0900

    [Gold V] Title: 토마토, Time: 108 ms, Memory: 9308 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,49 +1,29 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS 
 #include <queue>
 #include <stdio.h>
-#include <cstring>
- 
-#define MAX_SIZE 1000 + 1
- 
+
 using namespace std;
- 
+
 struct tomato {
     int y, x;
 };
- 
+
 queue<tomato> q;
- 
+
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0 , -1 };
- 
+
 int n, m, result = 0;
-int graph[MAX_SIZE][MAX_SIZE];
- 
+int graph[1001][1001];
+
 bool IsInside(int ny, int nx) {
-    return (0 <= nx && 0 <= ny && nx < m && ny < n);
+    return (0 <= nx && 0 <= ny && nx < m&& ny < n);
 }
- 
-void bfs(void) {
-    while (!q.empty()) {
-        int y = q.front().y;
-        int x = q.front().x;
-        q.pop();
- 
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
- 
-            if (IsInside(ny,nx) == 1 && graph[ny][nx] == 0) {
-                graph[ny][nx] = graph[y][x] + 1;
-                q.push({ ny, nx });
-            }
-        }
-    }
-}
- 
+
 int main() {
+    //freopen("input.txt", "rt", stdin);
     scanf("%d %d", &m, &n);
- 
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             scanf("%d", &graph[i][j]);
@@ -52,12 +32,26 @@ int main() {
             }
         }
     }
- 
-    bfs();
- 
+
+    while (!q.empty()) {
+        int y = q.front().y;
+        int x = q.front().x;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (IsInside(ny, nx) == 1 && graph[ny][nx] == 0) {
+                graph[ny][nx] = graph[y][x] + 1;
+                q.push({ ny, nx });
+            }
+        }
+    }
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (graph[i][j] == 0) { 
+            if (graph[i][j] == 0) {
                 printf("-1\n");
                 return 0;
             }
@@ -66,6 +60,6 @@ int main() {
             }
         }
     }
-    printf("%d\n", result-1);
+    printf("%d\n", result - 1);
     return 0;
 }
