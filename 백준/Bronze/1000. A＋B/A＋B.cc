e72180b399a7344137af59ea32cbb7ee30aed8a5
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-06-08 09:57:35 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a + b);
-    return 0;
+	
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a + b);
+	return 0;
 }
