d3425a70fd4282269cd42665b1bd9011fb5d7d13
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-06-08 14:32:00 +0900

    [Platinum II] Title: 계단 자르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Platinum/28287. 계단 자르기/계단 자르기.cc
+++ 백준/Platinum/28287. 계단 자르기/계단 자르기.cc
@@ -1,4 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include<stdio.h>
-#define l long long
-#define fr(A,S,E) for(int A=S;A<=E;A++)
-l c[103]={1,},d[103],m,n;int main(){scanf("%d%lld",&n,&m);fr(i,1,n+2)fr(j,0,i-1)c[i]=(c[i]+(c[j]*c[i-j-1]%m))%m;fr(i,1,n){d[i]=(2*c[i+1]+c[i]-c[i+2]+m)%m;fr(j,1,i-1)d[i]=(d[i]+((2*(i-j)+1)*c[j]%m*c[i-j]%m))%m;fr(j,1,i)d[i]=(d[i]+(((c[j-1]*d[i-j])+(d[j-1]*c[i-j]))%m))%m;}fr(i,1,n)printf("%lld ",d[i]);}
+
+long long int com[103], dp[103];
+
+int main() {
+    int n;
+    long long int mod;
+    scanf("%d %lld", &n, &mod);
+    com[0] = 1;
+    for (int i = 1; i <= n + 2; i++) {
+        for (int j = 0; j <= i - 1; j++) {
+            com[i] = (com[i] + (com[j] * com[i - j - 1] % mod)) % mod;
+        }
+    }
+    dp[1] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = (2 * com[i + 1] + com[i] - com[i + 2] + mod) % mod;
+
+        for (int j = 1; j <= i - 1; j++) {
+            dp[i] = (dp[i] + ((2 * (i - j) + 1) * com[j] % mod * com[i - j] % mod)) % mod;
+            dp[i] %= mod;
+        }
+
+        for (int j = 1; j <= i; j++) {
+            dp[i] = (dp[i] + (((com[j - 1] * dp[i - j]) + (dp[j - 1] * com[i - j])) % mod)) % mod;
+        }
+
+    }
+    for (int i = 1; i <= n; i++) {
+        printf("%lld ", dp[i]);
+    }
+    return 0;
+}
