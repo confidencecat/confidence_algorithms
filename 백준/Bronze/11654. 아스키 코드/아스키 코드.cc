caa123aba86a3f602f0f7826c91a363107d9908d
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-06-11 09:14:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -1,8 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main() {
-    char c;
-    scanf("%c", &c);
-    printf("%d", c);
+#include<stdio.h>
+
+int main(){
+    char a;
+    scanf("%c", &a);
+    printf("%d", a);
     return 0;
 }
