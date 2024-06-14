d37fe6c388bad91aa6253a4bc05e791fb7168f79
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-06-15 01:53:39 +0900

    [Gold IV] Title: 연구소, Time: 44 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,21 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
-int n, m, a[9][9], a1[9][9], mx = -100;
-int dx[4] = { 0, 0, -1, 1 };
+
+
+int n, m, fsafe_zone = 0, ans = 0;
+int fmap[9][9];
+int dx[4] = { 0, 0, 1, -1 };
 int dy[4] = { -1, 1, 0, 0 };
+vector<pair<int, int>> v;
+vector<pair<int, int>> qv;
 
 
-int bfs() {
-    queue<pair<int, int>> q;
-    int zn = 0;
+int bfs(int vx1, int vx2, int vx3) {
+    int map[9][9], safe_zone = fsafe_zone - 3;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (a1[i][j] == 2) q.push({ i, j });
-            if (a1[i][j] == 0) zn++;
+            map[i][j] = fmap[i][j];
         }
+    }
+
+    queue<pair<int, int>> q;
+    for (auto& fq : qv) {
+        q.push({ fq.first, fq.second });
     }
 
     while (!q.empty()) {
@@ -23,43 +31,31 @@ int bfs() {
         int y = q.front().second;
         q.pop();
 
-        for (int i = 0; i < 4; i++) {
-            int cx = x + dx[i];
-            int cy = y + dy[i];
-            if (cx < 0 || cx >= n || cy < 0 || cy >= m || a1[cx][cy] != 0) continue;
-            q.push({ cx, cy });
-            a1[cx][cy] = 2;
-            zn--;
-        }
-    }
-    return zn;
-}
+        //printf("%d %d\n", x, y);
 
-void mw(int wn) {
-    if (wn == 3) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 4; i++) {
+            int cx = dx[i] + x;
+            int cy = dy[i] + y;
+            //printf("%d %d\n", cx, cy);
+            if (cx < 0 || cx >= m || cy < 0 || cy >= n) continue;
+            
+            if (map[cy][cx] == 2 || map[cy][cx] == 1) continue;
+            q.push({ cx, cy });
+            map[cy][cx] = 2;
+            safe_zone--;
+        }
+
+        /*for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                a1[i][j] = a[i][j];
+                printf("%d ", map[i][j]);
             }
+            printf("\n");
         }
-        
-        int b = bfs();
-        if (b > mx) {
-            mx = b;
-        }
-        wn = 0;
+        printf("\n");*/
     }
-    else {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (a[i][j] == 0) {
-                    a[i][j] = 1;
-                    mw(wn + 1);
-                    a[i][j] = 0;
-                }
-            }
-        }
-    }
+    //printf("%d\n", safe_zone);
+    //printf("--------------------------\n");
+    return safe_zone;
 }
 
 int main() {
@@ -68,10 +64,33 @@ int main() {
     scanf("%d %d", &n, &m);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            scanf("%d", &a[i][j]);
+            scanf("%d", &fmap[i][j]);
+            if (fmap[i][j] == 0) {
+                v.push_back({ j, i });
+                fsafe_zone++;
+            }
+            if (fmap[i][j] == 2) qv.push_back({ j, i });
         }
     }
-    mw(0);
-    printf("%d\n", mx);
+
+    for (int i = 0; i < fsafe_zone - 2; i++) {
+        fmap[v[i].second][v[i].first] = 1;
+        for (int j = i + 1; j < fsafe_zone - 1; j++) {
+            fmap[v[j].second][v[j].first] = 1;
+            for (int k = j + 1; k < fsafe_zone; k++) {
+                fmap[v[k].second][v[k].first] = 1;
+
+                //printf("%d %d %d\n", i, j, k);
+                int ret = bfs(i, j, k);
+                if (ret > ans) ans = ret;
+
+                fmap[v[k].second][v[k].first] = 0;
+            }
+            fmap[v[j].second][v[j].first] = 0;
+        }
+        fmap[v[i].second][v[i].first] = 0;
+    }
+
+    printf("%d\n", ans);
     return 0;
 }
