866058697fb81ad5fe2509222032d7b905ee037b
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-06-15 00:33:20 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,34 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int a[15], n, ans = 0;
-
-bool ch(int x) {
-    for (int i = 0; i < x; i++) {
-        if (a[x] == a[i] || x - i == abs(a[x] - a[i])) return false;
-    }
-    return true;
-}
-
-void nq(int x) {
-    if (x == n) {
-        ans++;
-        return;
-    }
-
-    for (int i = 0; i < n; i++) {
-        a[x] = i;
-        if (ch(x)) {
-            nq(x + 1);
-        }
-    }
-}
+int n, a[15] = { 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596 };
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    
-    scanf("%d", &n);
-    nq(0);
-    printf("%d\n", ans);
+    scanf("%d\n", &n);
+    printf("%d", a[n - 1]);
     return 0;
 }
