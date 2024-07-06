6529d02f68cac637d7a9bf7f47fb7e4738274d20
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-06 14:07:13 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,22 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
 
-#define min(a, b) a > b ? b : a
+
+int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+int a, dp[1000001];
 
 int main() {
-	//freopen("input.txt", "rt", stdin);
-	int n, arr[1000001] = { 0 };
-	scanf("%d", &n);
+    //freopen("input.txt", "rt", stdin);
+    scanf("%d", &a);
 
-	for (int i = 2; i < n + 1; i++) {
-		arr[i] = arr[i - 1] + 1;
-		if (i % 2 == 0) {
-			arr[i] = min(arr[i / 2] + 1, arr[i]);
-		}
-		if (i % 3 == 0) {
-			arr[i] = min(arr[i / 3] + 1, arr[i]);
-		}  
-	}
-	printf("%d", arr[n]);
-	return 0;
+    
+    for (int i = 2; i <= a; i++) {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+    }
+    printf("%d\n", dp[a]);
+    return 0;
 }
