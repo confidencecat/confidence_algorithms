bbcf59057b14a2ecba2486597af560d83fd8a804
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-06 15:21:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,15 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
+
 
 int main() {
-	//freopen("input.txt", "rt", stdin);
+    //freopen("input.txt", "rt", stdin);
+    
+    long long int dp[1002] = { 0, 1, 2, };
 
-	int n, arr[1001] = { 1,1, };
-	scanf("%d", &n);
-	for (int i = 2; i <= n; i++) {
-		arr[i] = arr[i - 2] + arr[i - 1];
-		arr[i] %= 10007;
-	}
-	printf("%d", arr[n]);
-	return 0;
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 2] + dp[i - 1];
+        dp[i] %= 10007;
+    }
+    printf("%lld\n", dp[n]);
+
+    return 0;
 }
