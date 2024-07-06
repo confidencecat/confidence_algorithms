7ae3350d18f29c045b4f0e25aa972a4323c86f7c
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-06 11:30:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 1132 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,20 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int n;
+#include <stdio.h>
+
+int min(int a, int b) {
+    return (a > b ? b : a);
+}
+
+int dp[5001], n;
+
 int main() {
-	//freopen("input.txt", "rt", stdin);
-	
-	scanf("%d", &n);
-	int m = 0;
-	while (n >= 0) {
-		if (n % 5 == 0) {
-			m += (n / 5);
-			printf("%d", m);
-			return 0;
-		}
-		n -= 3;
-		m++;
-	}
-	printf("-1");
-	return 0;
+    //freopen("input.txt", "rt", stdin);
+
+    scanf("%d", &n);
+
+    
+    for (int i = 1; i <= n; i++) dp[i] = 5000;
+    dp[3] = dp[5] = 1;
+    for (int i = 6; i <= n; i++) {
+        dp[i] = min(dp[i - 3], dp[i - 5]) + 1;
+    }
+
+    printf("%d\n", dp[n] >= 5000 ? -1 : dp[n]);
+    return 0;
 }
