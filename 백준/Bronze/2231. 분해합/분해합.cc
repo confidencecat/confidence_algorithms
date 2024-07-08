0673b9143b1fe187a7bbcd3975414a448c5e5bc7
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-08 22:53:51 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,22 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
+
+int f(int n) {
+    int ans = n;
+    //printf("%d : ", n);
+    while (n > 0) {
+        ans += n % 10;
+        //printf("%d ", n % 10);
+        n /= 10;
+    }
+    return ans;
+}
 
 int main() {
-	//freopen("input.txt", "rt", stdin);
-	int n, m, ans = 0, a;
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++) {
-		m = i;
-		a = i;
-		while (m > 0) {
-			a += m % 10;
-			m /= 10;
-		}
-		if (a == n) {
-			ans = i;
-			break;
-		}
-	}
-	printf("%d\n", ans);
-	return 0;
+    //freopen("input.txt", "rt", stdin);
+
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (n == f(i)) {
+            printf("%d\n", i);
+            return 0;
+        }
+        //printf("\n");
+    }
+    printf("0");
+
+    return 0;
 }
