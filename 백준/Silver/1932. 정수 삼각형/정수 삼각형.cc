bd68bd2f3fcc3b4baf70a76f69bfc944660ce67c
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-08 21:28:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 16 ms, Memory: 3072 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -15,20 +15,14 @@ int main() {
     int n, ans = 0;
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            scanf("%d", &a[i][j]);
-        }
-    }
-
+    scanf("%d", &a[1][1]);
     ans = dp[1][1] = a[1][1];
     for (int i = 2; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
+            scanf("%d", &a[i][j]);
             dp[i][j] = a[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j]);
-            //printf("%d(%d) ", dp[i][j], max(dp[i - 1][j - 1], dp[i - 1][j]));
             if (dp[i][j] > ans) ans = dp[i][j];
         }
-        //printf("\n");
     }
 
     printf("%d\n", ans);
