086b6a8d5ba0f6cf3ffb2a1f3490b86f079f7441
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-08 21:26:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 16 ms, Memory: 3072 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,24 +1,37 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define max(a, b) a > b ? a : b
 
-int a[501][501], answer = 0, n;
+int max(int a, int b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+    return a < b ? b : a;
+}
+
+int a[501][501], dp[501][501];
 
 int main() {
     //freopen("input.txt", "rt", stdin);
+
+    int n, ans = 0;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i + 1; j++) {
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
             scanf("%d", &a[i][j]);
         }
     }
-    for (int i = n - 2; i >= 0; i--) {
-        for (int j = 0; j <= i; j++) {
-            a[i][j] += max(a[i + 1][j + 1], a[i + 1][j]);
+
+    ans = dp[1][1] = a[1][1];
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i][j] = a[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j]);
+            //printf("%d(%d) ", dp[i][j], max(dp[i - 1][j - 1], dp[i - 1][j]));
+            if (dp[i][j] > ans) ans = dp[i][j];
         }
+        //printf("\n");
     }
-    
-    printf("%d\n", a[0][0]);
+
+    printf("%d\n", ans);
+
     return 0;
 }
-
