b66dc670d71d97f132c21c45c1552aeb7a1da531
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-24 23:48:43 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 1276 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -3,48 +3,47 @@
 #include <queue>
 using namespace std;
 
-int n, m, a[101][101];
+int n, m;
 char map[101][101];
+int ch[101][101];
+
 int dx[4] = { 0, 0, -1, 1 };
-int dy[4] = { -1, 1, 0 , 0 };
+int dy[4] = { -1, 1, 0, 0 };
 
 int main() {
-    //freopen("input.txt", "rt", stdin);
+	//freopen("input.txt", "rt", stdin);
+	scanf("%d %d", &n, &m);
 
-    scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%s", map[i]);
+	}
 
-    for (int i = 0; i < n; i++) {
-        scanf("%s", map[i]);
-    }
+	queue<pair<int, int>> q;
 
-    queue<pair<int, int>> q;
+	ch[0][0] = 1;
+	q.push({ 0, 0 });
 
-    q.push({ 0, 0 });
-    a[0][0] = 1;
-    
-    while (!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
 
-        //printf("%d %d\n", x, y);
-
-        if (x == m && y == n) break;
-
-        for (int i = 0; i < 4; i++) {
-            int cx = dx[i] + x;
-            int cy = dy[i] + y;
-
-            //printf("%d %d\n", cx, cy);
-            if (cx < 0 || cx >= m || cy < 0 || cy >= n || map[cy][cx] == '0' || a[cy][cx] != 0) continue;
-
-            a[cy][cx] = a[y][x] + 1;
-            q.push({ cx, cy });
-        }
-
-    }
+		if (x == m - 1 && y == n - 1) {
+			
+			break;
+		}
+		for (int i = 0; i < 4; i++) {
+			int cx = x + dx[i];
+			int cy = y + dy[i];
+			if (cx < 0 || cx >= m || cy < 0 || cy >= n) continue;
+			if (ch[cy][cx] != 0 || map[cy][cx] == '0') continue;
+			ch[cy][cx] = ch[y][x] + 1;
+			q.push({ cx, cy });
+		}
+	}
 
 
-    printf("%d\n", a[n - 1][m - 1]);
-    return 0;
+	printf("%d\n", ch[n - 1][m - 1]);
+
+	return 0;
 }
