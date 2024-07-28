4c77d8825d8c08b0c9b354381855a83037b5ad83
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-28 19:53:47 +0900

    [Gold III] Title: 소수의 연속합, Time: 20 ms, Memory: 8336 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,39 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdbool.h>
 
-int n;
-bool a[4000001];
-int p[4000001], pn = 0;
-
-bool sum(int x) {
-	int s = 0;
-	while (x >= 0 && s < n) {
-		s += p[x--];
-		if (s == n) return true;
-	}
-	return false;
-}
+bool era[4000001];
+int prime[283148], pn = 0;
+long long int sum[283148];
 
 int main() {
-	//freopen("input.txt", "rt", stdin);
-	int ans = 0;
-	scanf("%d", &n);
-	for (int i = 2; i <= n; i++) {
-		if (!a[i]) {
-			for (int j = i; j <= n; j += i) {
-				if (i != j) {
-					a[j] = true;
-				}
-			}
-			p[pn++] = i;
-		}
-	}
+    //freopen("input.txt", "rt", stdin);
 
-	for (int i = pn - 1; i >= 0; i--) {
-		if (sum(i)) {
-			ans++;
-		}
-	}
-	printf("%d\n", ans);
-	return 0;
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 2; i * i <= n; i++) {
+        if (!era[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                era[j] = true;
+            }
+        }
+    }
+
+    for (int i = 2; i <= n; i++) {
+        if (!era[i]) {
+            prime[pn] = i;
+            sum[pn] = (pn == 0) ? i : sum[pn - 1] + i;
+            pn++;
+        }
+    }
+
+    int s = 0, e = 0;
+    int ans = 0;
+
+    while (e < pn) {
+        long long int sx = (s > 0) ? sum[e] - sum[s - 1] : sum[e];
+        if (sx > n) {
+            s++;
+        }
+        else if (sx < n) {
+            e++;
+        }
+        else {
+            ans++;
+            e++;
+        }
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
 }
