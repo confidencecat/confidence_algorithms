f2bf32541651b5408b63bfdadc192d87041dd4b4
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-28 23:55:52 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,25 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
 
-int dp[31][31];
+int n, a[32][32];
 
 int main() {
-    int t, n, m;
-    for (int i = 0; i < 30; i++) dp[0][i] = 1;
+    //freopen("input.txt", "rt", stdin);
+    
+    for (int i = 1; i <= 30; i++) a[1][i] = i;
+
+    for (int i = 2; i <= 30; i++) {
+        for (int j = i; j <= 30; j++) {
+            a[i][j] = a[i - 1][j - 1] + a[i][j - 1];
+        }
+    }
+
+    /*for (int i = 1; i <= 30; i++) {
+        for (int j = 1; j <= 30; j++) {
+            printf("%8d ", a[i][j]);
+        }
+        printf("\n");
+    }*/
+
+    int t, s, e;
     scanf("%d", &t);
+
     while (t--) {
-        scanf("%d %d", &n, &m);
-        if (n == 0 || m < n) {
-            printf("0\n");
-            continue;
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
-            }
-        }
-        printf("%d\n", dp[n][m]);
+        scanf("%d %d", &s, &e);
+        //printf("%d %d\n", s, e);
+        printf("%d\n", a[s][e]);
     }
     return 0;
 }
-//mCn
