e3e806ec80979e73e374c52de2b60dbcb205de14
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-30 22:29:37 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 204 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,31 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int a[1026][1026];
+int n, m, a;
+long long int d[1026][1026];
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int n, m, x1, y1, x2, y2;
-
+    
     scanf("%d %d", &n, &m);
-
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            scanf("%d", &a[i][j]);
-            a[i][j] = a[i][j] + a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1];
-
+            scanf("%d", &a);
+            d[i][j] = d[i - 1][j] + d[i][j - 1] - d[i - 1][j - 1] + a;
         }
     }
+
     /*for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            printf("%d ", a[i][j]);
+            printf("%2d ", d[i][j]);
         }
         printf("\n");
     }*/
-    for (int i = 0; i < m; i++) {
-        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-        printf("%d\n", a[x2][y2] - a[x1 - 1][y2] - a[x2][y1 - 1] + a[x1 - 1][y1 - 1]);
-    }
 
+
+    while (m--) {
+        int x1, y1, x2, y2;
+        scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
+
+        printf("%lld\n", d[y2][x2] - d[y1 - 1][x2] - d[y2][x1 - 1] + d[y1 - 1][x1 - 1]);
+    }
     return 0;
 }
