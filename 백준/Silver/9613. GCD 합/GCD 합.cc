2dc7630c74c2e08cdb2ec340a7387e5aec5bd9a3
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-30 19:48:23 +0900

    [Silver IV] Title: GCD 합, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.cc
+++ 백준/Silver/9613. GCD 합/GCD 합.cc
@@ -1,27 +1,56 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int com(const void* first, const void* second) {
+    return *(int*)first - *(int*)second;
+}
+
+bool ch[101];
+long long int ans = 0;
 
 int gcd(int a, int b) {
-	if (b) return gcd(b, a % b);
-	return a;
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+void f(int x, int fn, int n, int a[]) {
+    if (2 == fn) {
+        int q = -1, w;
+        for (int i = 0; i < n; i++) {
+            if (ch[i]) {
+                if (q == -1) q = a[i];
+                else w = a[i];
+                
+            }
+            //printf("%d ", ch[i]);
+        }
+        //printf("\n");
+        ans += gcd(q, w);
+    }
+    else if(x < n) {
+        ch[x] = true;
+        f(x + 1, fn + 1, n, a);
+        ch[x] = false;
+        f(x + 1, fn, n, a);
+    }
 }
 
 int main() {
-	//freopen("input.txt", "rt", stdin);
-	int t, n, arr[101];
-	long long int ans = 0;
-	scanf("%d", &t);
-	while (t--) {
-		ans = 0;
-		scanf("%d", &n);
-		scanf("%d", &arr[0]);
-		for (int i = 1; i < n; i++) {
-			scanf("%d", &arr[i]);
-			for (int j = 0; j < i; j++) {
-				ans += gcd(arr[i], arr[j]);
-			}
-		}
-		printf("%lld\n", ans);
-	}
-	return 0;
+    //freopen("input.txt", "rt", stdin);
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n, a[101];
+        scanf("%d", &n);
+        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+        qsort(a, n, sizeof(int), com);
+        ans = 0;
+        f(0, 0, n, a);
+        printf("%lld\n", ans);
+        
+    }
+
+    return 0;
 }
