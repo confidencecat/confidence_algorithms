b117e4c4f8ea9d73dfe97672909b32e48d5b89f4
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-31 00:43:25 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,23 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define max(a, b) a > b ? a : b
 
-int ans[16];
+int n, a[16][2];
+int dp[22];
 
 int main() {
     //freopen("input.txt", "rt", stdin);
     
-    int n, a[16][3];
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d %d", &a[i][0], &a[i][1]);
     }
+
     for (int i = 0; i < n; i++) {
-        ans[i + a[i][0]] = max(ans[i + a[i][0]], ans[i] + a[i][1]);
+        
+        dp[i + a[i][0]] = max(dp[i + a[i][0]], dp[i] + a[i][1]);
+        
         for (int j = i + a[i][0]; j <= n; j++) {
-            ans[j] = max(ans[j - 1], ans[j]);
+            dp[j] = max(dp[j - 1], dp[j]);
         }
     }
-    printf("%d\n", ans[n]);
+
+    printf("%d\n", dp[n]);
     return 0;
 }
