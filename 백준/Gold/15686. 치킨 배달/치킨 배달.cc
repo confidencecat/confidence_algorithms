16d966c2282bb905a31cc4c78f345c24fb5cd783
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-31 19:55:44 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,71 +1,69 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
 int n, m;
-int map[50][50];
-int houses[50][2], houses_n = 0;
-int chicken[50][2], chicken_n = 0;
-int selected[50];
+int city[50][50];
+int hs[50 * 2][2], hs_cnt = 0;
+int ck[50 * 2][2], ck_cnt = 0;
+int sel[50 * 2];
 
 int min(int a, int b) {
     return (a < b) ? a : b;
 }
 
-int fsum() {
-    int sum = 0;
-    for (int i = 0; i < houses_n; i++) {
-        int msum = INT_MAX;
-        for (int j = 0; j < chicken_n; j++) {
-            if (selected[j]) {
-                int d = abs(houses[i][0] - chicken[j][0]) + abs(houses[i][1] - chicken[j][1]);
-                msum = min(msum, d);
+int f() {
+    int ans = 0;
+    for (int i = 0; i < hs_cnt; i++) {
+        int dist = INT_MAX;
+        for (int j = 0; j < ck_cnt; j++) {
+            if (sel[j]) {
+                int d = abs(hs[i][0] - ck[j][0]) + abs(hs[i][1] - ck[j][1]);
+                dist = min(dist, d);
             }
         }
-        sum += msum;
+        ans += dist;
     }
-    return sum;
-}
-
-int f(int x, int fn) {
-    if (fn == m) {
-        return fsum();
-    }
-    if (x == chicken_n) {
-        return INT_MAX;
-    }
-
-    int ans = INT_MAX;
-
-    selected[x] = 1;
-    ans = min(ans, f(x + 1, fn + 1));
-
-    selected[x] = 0;
-    ans = min(ans, f(x + 1, fn));
-
     return ans;
 }
 
+int solve(int idx, int cnt) {
+    if (cnt == m) {
+        return f();
+    }
+    if (idx == ck_cnt) {
+        return INT_MAX;
+    }
+
+    int min_dist = INT_MAX;
+    sel[idx] = 1;
+    min_dist = min(min_dist, solve(idx + 1, cnt + 1));
+    sel[idx] = 0;
+    min_dist = min(min_dist, solve(idx + 1, cnt));
+
+    return min_dist;
+}
+
 int main() {
-    //freopen("input.txt", "rt", stdin);
     scanf("%d %d", &n, &m);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &map[i][j]);
-            if (map[i][j] == 1) {
-                houses[houses_n][0] = i;
-                houses[houses_n++][1] = j;
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < n; c++) {
+            scanf("%d", &city[r][c]);
+            if (city[r][c] == 1) {
+                hs[hs_cnt][0] = r;
+                hs[hs_cnt][1] = c;
+                hs_cnt++;
             }
-            else if (map[i][j] == 2) {
-                chicken[chicken_n][0] = i;
-                chicken[chicken_n++][1] = j;
+            else if (city[r][c] == 2) {
+                ck[ck_cnt][0] = r;
+                ck[ck_cnt][1] = c;
+                ck_cnt++;
             }
         }
     }
 
-    printf("%d\n", f(0, 0));
+    printf("%d\n", solve(0, 0));
 
     return 0;
 }
