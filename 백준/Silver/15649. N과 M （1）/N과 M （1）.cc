421332206f120a22ef533e82b918c586c3fc71a8
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-31 18:23:11 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int n, m;
+int ch[10];
+bool c[10];
+
+void f(int fn) {
+    if (fn == m) {
+        for (int i = 0; i < m; i++) {
+            printf("%d ", ch[i]);
+        }
+        printf("\n");
+    }
+    else if(fn < n) {
+        for (int i = 1; i <= n; i++) {
+            if (c[i]) continue;
+            ch[fn] = i;
+            c[i] = true;
+            f(fn + 1);
+            c[i] = false;
+        }
+    }
+}
+
+int main() {
+    //freopen("input.txt", "rt", stdin);
+
+    scanf("%d %d", &n, &m);
+
+    f(0);
+
+    return 0;
+}
