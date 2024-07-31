53edee0e04b68d3c581ce63d63b1e626d617a798
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-07-31 14:42:30 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 1136 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,68 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <queue>
-using namespace std;
 
-typedef struct {
-    int x, y, d;
-} Q;
+int n, m, x, y, d; //북동남서 0123
+int map[52][52], ch[52][52]; //0 청소 안함 1 청소함
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int ans = 0;
+
+bool ch4(int x, int y) { //true : 청소할 공간이 남음
+    for (int i = 0; i < 4; i++) {
+        int cx = x + dx[i];
+        int cy = y + dy[i];
+        if (cx < 0 || cx >= n || cy < 0 || cy >= m) continue;
+        if (map[cx][cy] != 1 && ch[cx][cy] == false) return true;
+    }
+    return false;
+}
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int n, m, rx, ry, rd, ans = 0;
-    int map[52][52];
-    int md[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
-    queue<Q> q;
 
     scanf("%d %d", &n, &m);
-    scanf("%d %d %d", &ry, &rx, &rd);
+    scanf("%d %d %d", &x, &y, &d);
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             scanf("%d", &map[i][j]);
         }
     }
 
-    q.push({ rx, ry, rd });
-
-    while (!q.empty()) {
-        int x = q.front().x;
-        int y = q.front().y;
-        int d = q.front().d;
-        q.pop();
-
-        if (map[y][x] == 0) {
-            map[y][x] = 2;
+    while (1) {
+        if (ch[x][y] == false) {
+            ch[x][y] = true;
             ans++;
         }
 
-        bool allCleaned = true;
-        for (int i = 0; i < 4; i++) {
-            int nextD = (d + 3) % 4;
-            int nx = x + md[nextD][1];
-            int ny = y + md[nextD][0];
-
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && map[ny][nx] == 0) {
-                q.push({ nx, ny, nextD });
-                allCleaned = false;
-                break;
+        if (ch4(x, y)) {
+            d = (d + 3) % 4;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 0 && ch[nx][ny] == false) {
+                x = nx;
+                y = ny;
             }
-            d = nextD;
         }
-
-        if (allCleaned) {
-            int backD = (d + 2) % 4;
-            int bx = x + md[backD][1];
-            int by = y + md[backD][0];
-
-            if (bx >= 0 && bx < m && by >= 0 && by < n && map[by][bx] != 1) {
-                q.push({ bx, by, d });
-            }
-            else {
-                break;
-            }
+        else {
+            int nx = x - dx[d];
+            int ny = y - dy[d];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || map[nx][ny] == 1) break;
+            x = nx;
+            y = ny;
         }
     }
 
     printf("%d\n", ans);
+
     return 0;
 }
