6636d67295822f18a93dd3dfa6d4256513784e75
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-08-01 21:29:00 +0900

    [Gold III] Title: 파티, Time: 4 ms, Memory: 1416 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,71 +1,79 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include <iostream>
-#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <queue>
-#include <climits>
+#include <vector>
+#define INF 200000000
 using namespace std;
 
-int answer[1001], answer1[1001];
+struct Q {
+    int e, w;
+
+    Q(int a, int b) {
+        e = a;
+        w = b;
+    }
+
+    bool operator<(const Q& q)const {
+        return w > q.w;
+    }
+};
+
+int n, m, st;
+int d[1001];
+int rd[1001];
+
+
+vector<pair<int, int>> vv[1001];
+vector<pair<int, int>> rv[1001];
+
+void dij(vector<pair<int, int>> v[], int arr[]) {
+    priority_queue<Q> q;
+    
+    for (int i = 1; i <= n; i++) arr[i] = INF;
+
+    arr[st] = 0;
+
+    q.push({ st, 0 });
+
+    while (!q.empty()) {
+        int x = q.top().e;
+        int w = q.top().w;
+        q.pop();
+
+        if (w > arr[x]) continue;
+
+        for (auto cx : v[x]) {
+            if (cx.second + w < arr[cx.first]) {
+                arr[cx.first] = cx.second + w;
+                q.push({ cx.first, cx.second + w });
+            }
+        }
+    }
+}
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    int v, e, s, nu, nv, nw, mx = 0;
-    scanf("%d %d %d", &v, &e, &s);
-    vector<pair<int, int>> a[1001], a1[1001];
+    
+    scanf("%d %d %d", &n, &m, &st);
 
-    for (int i = 1; i <= v; i++) {
-        answer1[i] = answer[i] = INT_MAX;
+    for (int i = 1; i <= n; i++) d[i] = INF;
+
+    for (int i = 0; i < m; i++) {
+        int ss, ee, W;
+        scanf("%d %d %d", &ss, &ee, &W);
+        vv[ss].push_back({ ee, W });
+        rv[ee].push_back({ ss, W });
     }
 
-    for (int i = 0; i < e; i++) {
-        scanf("%d %d %d", &nu, &nv, &nw);
-        a[nu].push_back(make_pair(nv, nw));
-        a1[nv].push_back(make_pair(nu, nw));
+    dij(vv, d);
+    dij(rv, rd); 
+
+    int ans = -1;
+    for (int i = 1; i <= n; i++) {
+        if (i == st) continue;
+        if (d[i] + rd[i] > ans) ans = d[i] + rd[i];
     }
+    printf("%d", ans);
 
-    answer[s] = 0;
-    priority_queue<pair<int, int>> q;
-    q.push(make_pair(0, s));
-
-    answer1[s] = 0;
-    priority_queue<pair<int, int>> q1;
-    q1.push(make_pair(0, s));
-
-    while (!q.empty() || !q1.empty()) {
-        if (!q.empty()) {
-            int d = -q.top().first;
-            int c = q.top().second;
-            q.pop();
-            if (answer[c] < d) continue;
-
-            for (auto& p : a[c]) {
-                int next = p.first;
-                int nextd = d + p.second;
-                if (nextd < answer[next]) {
-                    answer[next] = nextd;
-                    q.push(make_pair(-nextd, next));
-                }
-            }
-        }
-        if (!q1.empty()) {
-            int d = -q1.top().first;
-            int c = q1.top().second;
-            q1.pop();
-            if (answer1[c] < d) continue;
-
-            for (auto& p : a1[c]) {
-                int next = p.first;
-                int nextd = d + p.second;
-                if (nextd < answer1[next]) {
-                    answer1[next] = nextd;
-                    q1.push(make_pair(-nextd, next));
-                }
-            }
-        }
-    }
-    for (int i = 1; i <= v; i++) {
-        if (answer[i] + answer1[i] > mx) mx = answer[i] + answer1[i];
-    }
-    printf("%d\n", mx);
     return 0;
 }
