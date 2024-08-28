ee83bd17f115f55d106706aa95432544d8691402
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-08-29 01:02:52 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cc
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -3,12 +3,12 @@
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    
+
     int t, m;
 
     scanf("%d %d", &t, &m);
 
-    printf("%d %d\n", (t + 24 - ((m - 45) >> 31 & 1)) % 24, (m + 60 - 45) % 60);
+    printf("%d %d\n", (t + 24 - (m < 45)) % 24, (m + 60 - 45) % 60);
 
     return 0;
 }
