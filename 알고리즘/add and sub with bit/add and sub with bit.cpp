7d5b9e60dcc49f97b95789653c9f665e2ffe9e6c
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-09-23 19:33:24 +0900

    add, sub, mul, divi with bit.cpp

--- 알고리즘/add and sub with bit/add and sub with bit.cpp
+++ 알고리즘/add and sub with bit/add and sub with bit.cpp
@@ -15,14 +15,38 @@ int sub(int a, int b) {
     return add(a, add(~b, 1));
 }
 
+int mul(int a, int b) {
+    int result = 0;
+    while (b != 0) {
+        if (b & 1) {
+            result = result + a;
+        }
+        a <<= 1;
+        b >>= 1;
+    }
+    return result;
+}
+
+int divi(int a, int b) {
+    int result = 0;
+    while (a >= b) {
+        int shift = 0;
+        while ((b << (shift + 1)) <= a) {
+            shift++;
+        }
+        result += 1 << shift;
+        a -= b << shift;
+    }
+    return result;
+}
+
+
 int main() {
-    
     int a, b;
 
     scanf("%d %d", &a, &b);
 
-    printf("%d\n", add(a, b));
-    printf("%d\n", sub(a, b));
+    printf("add : %d\nsub : %d\nmul : %d\ndivi : %d\n", add(a, b), sub(a, b), mul(a, b), divi(a, b));
 
     return 0;
 }
