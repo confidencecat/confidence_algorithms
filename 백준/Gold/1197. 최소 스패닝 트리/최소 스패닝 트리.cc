ef0968fc4f275e711e52d41d34a060fd59867568
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-10-31 17:51:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 48 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,66 +1,52 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <queue>
 using namespace std;
 
-struct Q {
-    int s, e, v;
-
-    Q(int a, int b, int c) {
-        s = a;
-        e = b;
-        v = c;
-    }
-
-    bool operator<(const Q &q)const {
-        return v > q.v;
-    }
+typedef struct info {
+	int s, e, v;
 };
 
-int n, m, u = 0, ans = 0;
-int d[10002];
-priority_queue<Q> q;
+bool operator<(info A, info B) {
+	return A.v > B.v;
+}
+
+int d[10001];
 
 int find(int a) {
-    if (d[a] == a) return a;
-    else return d[a] = find(d[a]);
+	if (d[a] == a) return a;
+	return d[a] = find(d[a]);
 }
 
 void uni(int a, int b) {
-    a = find(a);
-    b = find(b);
-    if (a != b) d[b] = a;
+	int A = find(a);
+	int B = find(b);
+	if (A != B) d[B] = A;
 }
 
 int main() {
-    //freopen("input.txt", "rt", stdin);
+	//freopen("input.txt", "rt", stdin);
+	int n, m, ans = 0, count = 0;
+	priority_queue<info> q;
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) d[i] = i;
+	for (int i = 0; i < m; i++) {
+		int ss, ee, vv;
+		scanf("%d %d %d", &ss, &ee, &vv);
+		q.push({ ss, ee, vv });
+	}
+	while (!q.empty() && count < n) {
+		int s = q.top().s;
+		int e = q.top().e;
+		int v = q.top().v;
+		q.pop();
 
-    scanf("%d %d", &n, &m);
-
-    for (int i = 1; i <= n; i++) d[i] = i;
-
-    for (int i = 0; i < m; i++) {
-        int s, e, v;
-        scanf("%d %d %d", &s, &e, &v);
-        q.push({ s, e, v });
-    }
-
-    while (u < n - 1) {
-        int s = q.top().s;
-        int e = q.top().e;
-        int v = q.top().v;
-        q.pop();
-
-        //printf("%d\n", v);
-
-        if (find(s) != find(e)) {
-            uni(s, e);
-            u++;
-            ans += v;
-        }
-        //if () break;
-    }
-
-    printf("%d\n", ans);
-    return 0;
+		if (find(s) != find(e)) {
+			uni(s, e);
+			ans += v;
+			count++;
+		}
+	}
+	printf("%d\n", ans);
+	return 0;
 }
