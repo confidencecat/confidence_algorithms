f88eb342420209747ab85b26c36976095f99c87c
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-11-09 16:34:11 +0900

    Update lower_bound_function.cpp

--- 알고리즘/lower_upper_bound/lower_bound_function.cpp
+++ 알고리즘/lower_upper_bound/lower_bound_function.cpp
@@ -1,20 +1,15 @@
 int lower_bound(int list[], int len, int key) {
-	int start = 0, end = len - 1;
-	int ans_index = len - 1;
+    int start = 0, end = len - 1;
+    int ans_index = len;
 
-	while (start <= end) {
-		int mid = (start + end) / 2;
-		if (list[mid] > key) {
-			end = mid - 1;
-			ans_index = mid;
-		}
-		else if(list[mid] < key) {
-			start = mid + 1;
-		}
-		else {
-			ans_index = mid;
-			break;
-		}
-	}
-	return ans_index;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (list[mid] >= key) {
+            end = mid - 1;
+            ans_index = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return ans_index;
 }
