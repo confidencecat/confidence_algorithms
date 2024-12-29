8ac041d2b6f9c757bda5c5286d4d8f177d563366
confidencecat <89053337+confidencecat@users.noreply.github.com>
2024-12-29 14:42:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 20 ms, Memory: 1504 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,27 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define max(a, b) a > b ? a : b
+#include <utility>
+using namespace std;
 
 int n, k, dp[100001];
-
-typedef struct {
-    int w, v;
-}S;
-
-S a[100001];
+pair<int, int> a[101];
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-
+    
     scanf("%d %d", &n, &k);
+
     for (int i = 0; i < n; i++) {
-        scanf("%d %d", &a[i].w, &a[i].v);
+        scanf("%d %d", &a[i].first, &a[i].second);
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = k; j > 0; j--) {
-            if (j >= a[i].w) {
-                if (dp[j] < dp[j - a[i].w] + a[i].v) dp[j] = dp[j - a[i].w] + a[i].v;
+            if (j >= a[i].first) {
+                if (dp[j] < dp[j - a[i].first] + a[i].second) dp[j] = dp[j - a[i].first] + a[i].second;
             }
         }
     }
