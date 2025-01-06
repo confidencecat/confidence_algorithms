b0d80d7ac5eb93b2eeeef92ebfe7c8802c5c7342
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-01-06 12:04:42 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -2,26 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int com(const void* a, const void* b) {
-    return *(int*)a < *(int*)b;
-}
-int ccom(const void* a, const void* b) {
-    return *(int*)a > *(int*)b;
-}
+int coma(const void* a, const void* b) { return *(int*)a < *(int*)b; }
+int comb(const void* a, const void* b) { return *(int*)a > *(int*)b; }
 
 int main() {
     //freopen("input.txt", "rt", stdin);
-    
-    int n, a[51], b[51], ans = 0;
+
+    int n, sum = 0, a[51], b[51];
+
     scanf("%d", &n);
     for (int i = 0; i < n; i++) scanf("%d", &a[i]);
     for (int i = 0; i < n; i++) scanf("%d", &b[i]);
 
-    qsort(a, n, sizeof(int), ccom);
-    qsort(b, n, sizeof(int), com);
+    qsort(a, n, sizeof(int), coma);
+    qsort(b, n, sizeof(int), comb);
 
-    for (int i = 0; i < n; i++) ans += a[i] * b[i];
-
-    printf("%d\n", ans);
+    for (int i = 0; i < n; i++) sum += a[i] * b[i];
+    printf("%d\n", sum);
     return 0;
 }
