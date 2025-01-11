86a7fcb10e92396a71d330e969c7f99da0482e65
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-01-11 23:16:06 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,54 +1,28 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-    int n, m;
-    int arr[51], a[51];
-    int ans = 0;
-    scanf("%d %d", &n, &m);
+int chk[55];
 
-    for (int i = 0; i < n; i++) {
-        arr[i] = i + 1;
-    }
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &a[i]);
-    }
+int main()
+{
+	int N, M, res = 0, cur;
+	scanf("%d %d", &N, &M);
 
-    int size = n;
-    for (int i = 0; i < m; i++) {
-        int pos = 0;
-        for (int j = 0; j < size; j++) {
-            if (arr[j] == a[i]) {
-                pos = j;
-                break;
-            }
-        }
-        if (pos <= size - pos - 1) {
-            ans += pos;
-            while (pos--) {
-                int temp = arr[0];
-                for (int k = 0; k < size - 1; k++) {
-                    arr[k] = arr[k + 1];
-                }
-                arr[size - 1] = temp;
-            }
-        }
-        else {
-            ans += (size - pos);
-            for (int j = 0; j < size - pos; j++) {
-                int temp = arr[size - 1];
-                for (int k = size - 1; k > 0; k--) {
-                    arr[k] = arr[k - 1];
-                }
-                arr[0] = temp;
-            }
-        }
-        for (int j = 0; j < size - 1; j++) {
-            arr[j] = arr[j + 1];
-        }
-        size--;
-    }
-
-    printf("%d\n", ans);
-    return 0;
+	cur = 0;
+	for (int i = 0; i < M; i++)
+	{
+		int target;
+		scanf("%d", &target);
+		target--;
+		int data = 0;
+		while (cur != target)
+		{
+			if (chk[cur] == 0)
+				data++;
+			cur = (cur + 1) % N;
+		}
+		if (N - i - data > data) res += data;
+		else res += N - i - data;
+		chk[cur] = 1;
+	}
+	printf("%d", res);
 }
