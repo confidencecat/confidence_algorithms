cdedaf6edaafdf835d08aa5343df114bab937587
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-01-21 17:14:50 +0900

    [Gold V] Title: 주사위, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/1041. 주사위/주사위.cc
+++ 백준/Gold/1041. 주사위/주사위.cc
@@ -10,11 +10,8 @@ int main() {
     scanf("%lld %lld %lld %lld %lld %lld %lld", &n, &num[0], &num[1], &num[2], &num[3], &num[4], &num[5]);
     
     if (n == 1) {
-        int sum = 0, mx = 0;
-        for (int i = 0; i < 6; i++) {
-            mx = mx < num[i] ? num[i] : mx;
-            sum += num[i];
-        }
+        int sum = num[0], mx = num[0];
+        for (int i = 0; i < 6; i++, mx = mx < num[i] ? num[i] : mx, sum += num[i]);
         printf("%d\n", sum - mx);
         return 0;
     }
@@ -24,18 +21,13 @@ int main() {
     int y[4] = { 2, 4, 3, 1 };
     min = min1 = num[0] > num[5] ? num[5] : num[0];
     for (int i = 1; i <= 4; i++) {
+        int d = (bcde - num[x[i - 1]] - num[y[i - 1]]);
         min1 = min1 > num[i] ? num[i] : min1;
-        min2 = min2 > (bcde - num[x[i - 1]] - num[y[i - 1]]) ? (bcde - num[x[i - 1]] - num[y[i - 1]]) : min2;
+        min2 = min2 > d ? d : min2;
         min2 = min2 > min + num[i] ? min + num[i] : min2;
-        min3 = min3 > (bcde - num[x[i - 1]] - num[y[i - 1]]) ? (bcde - num[x[i - 1]] - num[y[i - 1]]) : min3;
-        //printf("--%lld = %lld - %lld - %lld,,,, %d %d\n", min3, bcde, num[x[i - 1]], num[y[i - 1]], i, i % 4 + 1);
+        min3 = min3 > d ? d : min3;
     }
-    min3 += min;
-
-    //printf("%lld %lld %lld %lld\n", min3, min2, min1, min);
-    
-    //printf("%lld %lld %lld\n", min3 * 4, min2 * ((n - 1) * 4 + (n - 2) * 4), min1 * ((n - 2) * (n - 2) + (n - 2) * (n - 1) * 4));
-    printf("%lld\n", min3 * 4 + min2 * ((n - 1) * 4 + (n - 2) * 4) + min1 * ((n - 2) * (n - 2) + (n - 2) * (n - 1) * 4));
+    printf("%lld\n", (min3+min) * 4 + min2 * ((n - 1) * 4 + (n - 2) * 4) + min1 * ((n - 2) * (n - 2) + (n - 2) * (n - 1) * 4));
     
     return 0;
 }
