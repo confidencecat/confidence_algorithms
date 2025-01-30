3c937a4752973ba77b2ee33355f6d382a84547d0
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-01-30 12:31:15 +0900

    [Bronze III] Title: 전자레인지, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+    if (t % 10 != 0) printf("-1");
+    else {
+        printf("%d %d %d", t / 300, (t % 300) / 60, ((t % 300) % 60) / 10);
+    }
+
+
+    return 0;
+}
