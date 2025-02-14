0501119805ac6c1cef8ae19743268a9046b189c5
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-02-14 14:10:42 +0900

    [Unrated] Title: 0.1, Time: 72 ms, Memory: 41572 KB, Score: 5000000 point -BaekjoonHub

--- 백준/Unrated/11921. 0．1/0．1.cc
+++ 백준/Unrated/11921. 0．1/0．1.cc
@@ -1,132 +1,126 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+using namespace std;
 
-//============================
+/////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Author : jinhan814
+ * Date : 2021-05-06
+ * Source : https://blog.naver.com/jinhan814/222266396476
+ * Description : FastIO implementation for cin, cout. (mmap ver.)
+ */
+constexpr int SZ = 1 << 20;
 
-#include <stdlib.h>
-#include <string.h>
-
-#define IBUF_SIZE (1 << 20)
-#define OBUF_SIZE (1 << 20)
-
-static char inbuf[IBUF_SIZE];
-static char* in_ptr, * in_end;
-static char outbuf[OBUF_SIZE];
-static int out_pos;
-
-void init() {
-	in_ptr = in_end = inbuf;
-	out_pos = 0;
-}
-
-static inline char scanfc() {
-	if (in_ptr == in_end) {
-		int bytes = (int)fread(inbuf, 1, IBUF_SIZE, stdin);
-		if (!bytes) return 0;
-		in_ptr = inbuf;
-		in_end = inbuf + bytes;
+class INPUT {
+private:
+	char* p;
+	bool __END_FLAG__, __GETLINE_FLAG__;
+public:
+	explicit operator bool() { return !__END_FLAG__; }
+    INPUT() {
+        struct stat st; fstat(0, &st);
+        p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
+    }
+	bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+	bool IsEnd(char c) { return c == '\0'; }
+	char _ReadChar() { return *p++; }
+	char ReadChar() {
+		char ret = _ReadChar();
+		for (; IsBlank(ret); ret = _ReadChar());
+		return ret;
 	}
-	return *in_ptr++;
-}
-
-int scanfi() {
-	int c = scanfc();
-	while (c == ' ' || c == '\n' || c == '\r' || c == '\t') c = scanfc();
-	int sign = 1;
-	if (c == '-') { sign = -1; c = scanfc(); }
-	int x = 0;
-	while (c >= '0' && c <= '9') {
-		x = x * 10 + (c - '0');
-		c = scanfc();
+	template<typename T> T ReadInt() {
+		T ret = 0; char cur = _ReadChar(); bool flag = 0;
+		for (; IsBlank(cur); cur = _ReadChar());
+		if (cur == '-') flag = 1, cur = _ReadChar();
+		for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret = 10 * ret + (cur & 15);
+		if (IsEnd(cur)) __END_FLAG__ = 1;
+		return flag ? -ret : ret;
 	}
-	return sign * x;
-}
-
-long long scanfl() {
-	int c = scanfc();
-	while (c == ' ' || c == '\n' || c == '\r' || c == '\t') c = scanfc();
-	int sign = 1;
-	if (c == '-') { sign = -1; c = scanfc(); }
-	long long x = 0;
-	while (c >= '0' && c <= '9') {
-		x = x * 10LL + (c - '0');
-		c = scanfc();
+	string ReadString() {
+		string ret; char cur = _ReadChar();
+		for (; IsBlank(cur); cur = _ReadChar());
+		for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+		if (IsEnd(cur)) __END_FLAG__ = 1;
+		return ret;
 	}
-	return sign * x;
-}
-
-char* scanfs() {
-	int cap = 128, len = 0;
-	char* s = (char*)malloc(cap);
-	int c = scanfc();
-	while (c == ' ' || c == '\n' || c == '\r' || c == '\t') c = scanfc();
-	while (c && c != ' ' && c != '\n' && c != '\r' && c != '\t') {
-		if (len + 1 >= cap) {
-			cap <<= 1;
-			s = (char*)realloc(s, cap);
-		}
-		s[len++] = c;
-		c = scanfc();
+	double ReadDouble() {
+		string ret = ReadString();
+		return stod(ret);
 	}
-	s[len] = '\0';
-	return s;
-}
-
-void bflush() {
-	if (out_pos) {
-		fwrite(outbuf, 1, out_pos, stdout);
-		out_pos = 0;
+	string getline() {
+		string ret; char cur = _ReadChar();
+		for (; cur != '\n' && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+		if (__GETLINE_FLAG__) __END_FLAG__ = 1;
+		if (IsEnd(cur)) __GETLINE_FLAG__ = 1;
+		return ret;
 	}
-}
+	friend INPUT& getline(INPUT& in, string& s) { s = in.getline(); return in; }
+} _in;
 
-void printfc(char c) {
-	if (out_pos == OBUF_SIZE) bflush();
-	outbuf[out_pos++] = c;
-}
-
-void printfi(int n) {
-	if (n == 0) { printfc('0'); return; }
-	if (n < 0) { printfc('-'); n = -n; }
-	char buf[12];
-	int idx = 0;
-	while (n) {
-		buf[idx++] = (n % 10) + '0';
-		n /= 10;
+class OUTPUT {
+private:
+	char write_buf[SZ];
+	int write_idx;
+public:
+	~OUTPUT() { Flush(); }
+	explicit operator bool() { return 1; }
+	void Flush() {
+        write(1, write_buf, write_idx);
+		write_idx = 0;
 	}
-	while (idx--) printfc(buf[idx]);
-}
-
-void printfl(long long n) {
-	if (n == 0) { printfc('0'); return; }
-	if (n < 0) { printfc('-'); n = -n; }
-	char buf[21];
-	int idx = 0;
-	while (n) {
-		buf[idx++] = (n % 10) + '0';
-		n /= 10;
+	void WriteChar(char c) {
+		if (write_idx == SZ) Flush();
+		write_buf[write_idx++] = c;
 	}
-	while (idx--) printfc(buf[idx]);
-}
+	template<typename T> int GetSize(T n) {
+		int ret = 1;
+		for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
+		return ret;
+	}
+	template<typename T> void WriteInt(T n) {
+		int sz = GetSize(n);
+		if (write_idx + sz >= SZ) Flush();
+		if (n < 0) write_buf[write_idx++] = '-', n = -n;
+		for (int i = sz; i --> 0; n /= 10) write_buf[write_idx + i] = n % 10 | 48;
+		write_idx += sz;
+	}
+	void WriteString(string s) { for (auto& c : s) WriteChar(c); }
+	void WriteDouble(double d) { WriteString(to_string(d)); }
+} _out;
 
-void printfs(const char* s) {
-	while (*s) printfc(*s++);
-}
+/* operators */
+INPUT& operator>> (INPUT& in, char& i) { i = in.ReadChar(); return in; }
+INPUT& operator>> (INPUT& in, string& i) { i = in.ReadString(); return in; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+INPUT& operator>> (INPUT& in, T& i) {
+	if constexpr (is_floating_point_v<T>) i = in.ReadDouble();
+	else if constexpr (is_integral_v<T>) i = in.ReadInt<T>(); return in; }
 
-//============================
+OUTPUT& operator<< (OUTPUT& out, char i) { out.WriteChar(i); return out; }
+OUTPUT& operator<< (OUTPUT& out, string i) { out.WriteString(i); return out; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+OUTPUT& operator<< (OUTPUT& out, T i) {
+	if constexpr (is_floating_point_v<T>) out.WriteDouble(i);
+	else if constexpr (is_integral_v<T>) out.WriteInt<T>(i); return out; }
+
+/* macros */
+#define fastio 1
+#define cin _in
+#define cout _out
+#define istream INPUT
+#define ostream OUTPUT
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+using ull = unsigned long long;
 
 int main() {
-	init();
-
-	int n = scanfi();
-	long long int ans = 0;
-	printfi(n); printfc('\n');
-	while (n--) {
-		ans += scanfi();
-	}
-
-	printfl(ans);
-
-	bflush();
-
-	return 0;
+	fastio;
+    ull n, t, sum = 0; cin >> n;
+    for (int i = 0; i < n; i++) cin >> t, sum += t;
+    cout << n << '\n' << sum << '\n';
 }
