264abaceb3cbf3dbaffd8e933b50128502ae7bca
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-02-14 13:28:24 +0900

    [Silver II] Title: 주식, Time: 104 ms, Memory: 10984 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -2,33 +2,149 @@
 #include <stdio.h>
 
 int t, n;
-long long int a[1000001], tn = 0;
-long long int ans = 0, sum = 0;
+long long int a[1000001], ans = 0;
 
+//============================
+#include <stdlib.h>
+#include <string.h>
+
+#define SZ (1 << 20)
+
+typedef struct {
+    char* read_buf;
+    int read_idx, next_idx;
+} INPUT;
+
+typedef struct {
+    char* write_buf;
+    int write_idx;
+} OUTPUT;
+
+INPUT input;
+OUTPUT output;
+
+void init_input() {
+    input.read_buf = (char*)malloc(SZ);
+    input.read_idx = input.next_idx = 0;
+}
+
+char scanfc() {
+    if (input.read_idx == input.next_idx) {
+        input.next_idx = fread(input.read_buf, 1, SZ, stdin);
+        if (!input.next_idx) return 0;
+        input.read_idx = 0;
+    }
+    return input.read_buf[input.read_idx++];
+}
+
+int is_blank(char c) { return c == ' ' || c == '\n' || c == '\r' || c == '\t'; }
+
+int scanfi() {
+    int result = 0, sign = 1;
+    char c = scanfc();
+    while (is_blank(c)) c = scanfc();
+    if (c == '-') { sign = -1; c = scanfc(); }
+    while (c >= '0' && c <= '9') { result = result * 10 + (c - '0'); c = scanfc(); }
+    return result * sign;
+}
+
+long long scanfl() {
+    long long result = 0;
+    int sign = 1;
+    char c = scanfc();
+    while (is_blank(c)) c = scanfc();
+    if (c == '-') { sign = -1; c = scanfc(); }
+    while (c >= '0' && c <= '9') { result = result * 10 + (c - '0'); c = scanfc(); }
+    return result * sign;
+}
+
+char* scanfs() {
+    char* str = (char*)malloc(SZ);
+    int i = 0;
+    char c = scanfc();
+    while (is_blank(c)) c = scanfc();
+    while (!is_blank(c) && c) {
+        str[i++] = c;
+        c = scanfc();
+    }
+    str[i] = '\0';
+    return str;
+}
+
+void cleanup_input() { free(input.read_buf); }
+
+void init_output() {
+    output.write_buf = (char*)malloc(SZ);
+    output.write_idx = 0;
+}
+
+void bflush() {
+    if (output.write_idx) {
+        fwrite(output.write_buf, 1, output.write_idx, stdout);
+        output.write_idx = 0;
+    }
+}
+
+void printfc(char c) {
+    if (output.write_idx == SZ) bflush();
+    output.write_buf[output.write_idx++] = c;
+}
+
+void printfi(int n) {
+    if (!n) { printfc('0'); return; }
+    if (n < 0) { printfc('-'); n = -n; }
+    char temp[12];
+    int i = 0;
+    while (n) { temp[i++] = n % 10 + '0'; n /= 10; }
+    while (i--) printfc(temp[i]);
+}
+
+void printfl(long long n) {
+    if (!n) { printfc('0'); return; }
+    if (n < 0) { printfc('-'); n = -n; }
+    char temp[20];
+    int i = 0;
+    while (n) { temp[i++] = n % 10 + '0'; n /= 10; }
+    while (i--) printfc(temp[i]);
+}
+
+void printfs(const char* str) {
+    while (*str) {
+        printfc(*str++);
+    }
+}
+
+void cleanup_output() {
+    bflush();
+    free(output.write_buf);
+}
+
+//============================
 
 int main() {
-    
-    for (scanf("%d", &t); t--;) {
-        scanf("%d", &n);
-        ans = 0, sum = 0, tn = 0;
+    init_input();
+    init_output();
+
+    t = scanfi();
+    while (t--) {
+        n = scanfi();
+        ans = 0;
 
         for (int i = 0; i < n; i++) {
-            scanf("%lld", &a[i]);
+            a[i] = scanfl();
         }
-        tn = n - 1;
-        while(tn > 1) {
-            int j = tn - 1;
-            for (; j >= 0; j--) {
-                if (a[j] < a[tn]) {
-                    ans += a[tn] - a[j];
-                }
-                else break;
-            }
-            tn = j;
+        
+        long long int mx = a[n - 1];
+        for (int j = n - 2; j >= 0; j--) {
+            if (a[j] >= mx) mx = a[j];
+            else ans += mx - a[j];
         }
-
-        printf("%lld\n", ans);
+        printfl(ans);
+        printfc('\n');
     }
+
+    cleanup_input();
+    cleanup_output();
 
     return 0;
 }
