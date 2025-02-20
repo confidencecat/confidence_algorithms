a2674a842755493dd4b99c867c42adf3ad86cea5
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-02-20 23:33:22 +0900

    [Gold II] Title: Урок физкультуры, Time: 24 ms, Memory: 1208 KB -BaekjoonHub

--- 백준/Gold/29132. Урок физкультуры/Урок физкультуры.cc
+++ 백준/Gold/29132. Урок физкультуры/Урок физкультуры.cc
@@ -1,40 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define MOD 1000000007
 
-int main(){
-    int n;
+int n;
+int a[5000], nondec[5000], noninc[5000];
+long long dp[5001];
+
+int main() {
     scanf("%d", &n);
-    
-    int a[5000];
-    for(int i = 0; i < n; i++){
-        scanf("%d", &a[i]);
-    }
-    
-    int nondec[5000], noninc[5000];
+
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
     nondec[n - 1] = n - 1;
     noninc[n - 1] = n - 1;
-    for(int i = n - 2; i >= 0; i--){
-        if(a[i] <= a[i + 1])
-            nondec[i] = nondec[i + 1];
-        else
-            nondec[i] = i;
-        
-        if(a[i] >= a[i + 1])
-            noninc[i] = noninc[i + 1];
-        else
-            noninc[i] = i;
+    for (int i = n - 2; i >= 0; i--) {
+        nondec[i] = a[i] <= a[i + 1] ? nondec[i + 1] : i;
+        noninc[i] = a[i] >= a[i + 1] ? noninc[i + 1] : i;
     }
-    
-    long long dp[5001] = {0};
+
     dp[0] = 1;
-    
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         int r = (nondec[i] > noninc[i]) ? nondec[i] : noninc[i];
-        for(int j = i; j <= r; j++){
+        for (int j = i; j <= r; j++) {
             dp[j + 1] = (dp[j + 1] + dp[i]) % MOD;
         }
     }
-    
+
     printf("%lld\n", dp[n]);
     return 0;
 }
+
