3548642fca079c0f9ff4ff8d49903d0929ab787b
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-02-21 13:34:08 +0900

    [Bronze I] Title: 컵홀더, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2810. 컵홀더/컵홀더.cc
+++ 백준/Bronze/2810. 컵홀더/컵홀더.cc
@@ -1,22 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int n, ans = 0;
+int n, t = 0;
 char s[51];
 
 int main() {
     scanf("%d", &n);
     scanf("%s", s);
-
-    for (int i = 0; i < n; i++) {
-        ans++;
-        if (s[i] == 'L') i++;
-    }
-    for (int i = n - 2; i >= 0; i--) {
-        if (s[i + 1] == 'S' && s[i] == 'L') { ans++; break; }
-    }
-    printf("%d\n", ans);
-
+    for (int i = 0; i < n; i++) { if (s[i] == 'L') t++; }
+    if (!t) printf("%d", n);
+    else printf("%d", n - t / 2 + 1);
     return 0;
 }
 
