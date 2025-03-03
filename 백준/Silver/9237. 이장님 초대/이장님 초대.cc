e6886037ca5652e95813a0164eed4234b8d45f1e
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-03-03 13:30:30 +0900

    [Silver V] Title: 이장님 초대, Time: 12 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/9237. 이장님 초대/이장님 초대.cc
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.cc
@@ -2,22 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int n, a[100001], c = 0, mi = 1<<31, mx = 0;
+char rbuf[1 << 20];
+int idx, nidx;
+inline char read() {
+    if (idx == nidx) {
+        nidx = fread(rbuf, 1, 1 << 20, stdin);
+        if (!nidx) return 0;
+        idx = 0;
+    }
+    return rbuf[idx++];
+}
+
+inline int ri() {
+    int sum = 0;
+    char now = read();
+
+    while (now <= 32) now = read();
+    while (now >= 48) sum = sum * 10 + now - '0', now = read();
+
+    return sum;
+}
+
+int n, i, a[100001], mx = 0;
 
 int com(const void* A, const void* B) {
     return *(int*)B - *(int*)A;
 }
 
-int main() {
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
+int main() {    
+    for (n=ri(), i =0; i < n; i++) a[i] = ri();
     qsort(a, n, sizeof(a[0]), com);
 
     for (int i = 0; i < n; i++) {
-        a[i] = a[i] - (n - i - 1);
-        mx = mx < a[i] ? a[i] : mx;
+        if ((a[i] - (n - i - 1)) > mx) mx = a[i] - (n - i - 1);
     }
     printf("%d\n", mx + n + 1);
     return 0;
