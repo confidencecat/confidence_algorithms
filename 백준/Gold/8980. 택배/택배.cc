eb59b45ec3748914ab5d685e7dad68df704af2b9
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-03-22 13:21:30 +0900

    [Gold II] Title: 택배, Time: 12 ms, Memory: 1236 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.cc
+++ 백준/Gold/8980. 택배/택배.cc
@@ -8,13 +8,12 @@ int com(const void* A, const void* B) {
     int* AA = (int*)A;
     int* BB = (int*)B;
     if (AA[1] == BB[1]) {
-        if (AA[0] == BB[0]) return BB[2] - AA[2];
-        else return AA[0] - BB[0];
+        return AA[0] - BB[0];
     }
     return AA[1] - BB[1];
 }
 
-int n, c, m, a[2001][3];
+int n, c, m, a[10001][3];
 int ch[2001], ans = 0;
 
 int main() {
@@ -23,19 +22,18 @@ int main() {
     scanf("%d", &m);
     for (int i = 0; i < m; i++) {
         scanf("%d %d %d", &a[i][0], &a[i][1], &a[i][2]);
-        a[i][2] = min(a[i][2], c);
     }
     qsort(a, m, sizeof(a[0]), com);
 
     for (int i = 0; i < m; i++) {
-        int mx = 0;
+        int canCarry = a[i][2];
         for (int j = a[i][0]; j < a[i][1]; j++) {
-            mx = max(mx, ch[j]);
+            canCarry = min(canCarry, c - ch[j]);
         }
         for (int j = a[i][0]; j < a[i][1]; j++) {
-            ch[j] += min(a[i][2], c - mx);
+            ch[j] += canCarry;
         }
-        ans += min(a[i][2], c - mx);
+        ans += canCarry;
     }
     printf("%d\n", ans);
 
