61ef786de7f5a50ce85672d81f4763715a7111dd
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-04-04 22:42:38 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 1232 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+int n, w, l;
+int a[1001];
+int ans = 0, sum = 0;
+queue<int> q;
+
+
+int main() {
+    scanf("%d %d %d", &n, &w, &l);
+
+    for (int i = 0; i < w; i++) q.push(0);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; ans++) {
+        sum -= q.front();
+        q.pop();
+
+        if (sum + a[i] <= l) {
+            q.push(a[i]);
+            sum += a[i++];
+        }
+        else {
+            q.push(0);
+        }
+    }
+    printf("%d\n", ans + w);
+
+
+
+    return 0;
+}
