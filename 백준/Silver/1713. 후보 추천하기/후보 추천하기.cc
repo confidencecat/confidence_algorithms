37c3fd716fe2bba2e69fc28aaf6c7af7959492d1
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-04-07 21:57:58 +0900

    [Silver I] Title: 후보 추천하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+typedef struct QQ {
+    int n, i, *c;
+}Q;
+
+int m, n, a[1001], c[101], dn = 0;
+Q d[21];
+
+int com(const void* A, const void* B) {
+    Q* AA = (Q*)A;
+    Q* BB = (Q*)B;
+    if (*(AA->c) == *(BB->c)) return AA->i - BB->i;
+    return *(AA->c) - *(BB->c);
+}
+
+int com_a(const void* A, const void* B) {
+    Q *AA = (Q*)A;
+    Q *BB = (Q*)B;
+    return AA->n - BB->n;
+}
+
+int main() {
+    scanf("%d", &m);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+    for (int i = 0; i < n; i++) {
+        if (dn == m) {
+            if (c[a[i]] == 0) {
+                c[d[0].n] = 0;
+                d[0] = Q{ a[i], i, &c[a[i]] };
+                c[a[i]] = 1;
+            }
+            else {
+                c[a[i]]++;
+            }
+        }
+        else {
+            if (c[a[i]] == 0) {
+                d[dn++] = Q{ a[i], i, &c[a[i]] };
+                c[a[i]] = 1;
+            }
+            else {
+                c[a[i]]++;
+            }
+        }
+        qsort(d, dn, sizeof(d[0]), com);
+
+        /*for (int i = 0; i < dn; i++) {
+            printf("%d %d %d\n", d[i].n, d[i].i, *d[i].c);
+        }
+        printf("\n");*/
+
+    }
+    qsort(d, dn, sizeof(d[0]), com_a);
+    for (int i = 0; i < dn; i++) {
+        printf("%d ", d[i].n);
+    }
+
+    return 0;
+}
