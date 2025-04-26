88b88519371763c81f18d6ab560e5f95bca35c81
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-04-26 20:39:32 +0900

    [Silver IV] Title: 문자열 집합, Time: 40 ms, Memory: 13536 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -3,11 +3,91 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define IBUF_SIZE (1 << 20)
+#define OBUF_SIZE (1 << 20)
+
+static char inbuf[IBUF_SIZE];
+static char* in_ptr, * in_end;
+static char outbuf[OBUF_SIZE];
+static int out_pos;
+
+void init() {
+	in_ptr = in_end = inbuf;
+	out_pos = 0;
+}
+
+static inline char scanfc() {
+	if (in_ptr == in_end) {
+		int bytes = (int)fread(inbuf, 1, IBUF_SIZE, stdin);
+		if (!bytes) return 0;
+		in_ptr = inbuf;
+		in_end = inbuf + bytes;
+	}
+	return *in_ptr++;
+}
+
+int scanfi() {
+	int c = scanfc();
+	while (c == ' ' || c == '\n' || c == '\r' || c == '\t') c = scanfc();
+	int sign = 1;
+	if (c == '-') { sign = -1; c = scanfc(); }
+	int x = 0;
+	while (c >= '0' && c <= '9') {
+		x = x * 10 + (c - '0');
+		c = scanfc();
+	}
+	return sign * x;
+}
+
+char* scanfs() {
+	int cap = 128, len = 0;
+	char* s = (char*)malloc(cap);
+	int c = scanfc();
+	while (c == ' ' || c == '\n' || c == '\r' || c == '\t') c = scanfc();
+	while (c && c != ' ' && c != '\n' && c != '\r' && c != '\t') {
+		if (len + 1 >= cap) {
+			cap <<= 1;
+			s = (char*)realloc(s, cap);
+		}
+		s[len++] = c;
+		c = scanfc();
+	}
+	s[len] = '\0';
+	return s;
+}
+
+void bflush() {
+	if (out_pos) {
+		fwrite(outbuf, 1, out_pos, stdout);
+		out_pos = 0;
+	}
+}
+
+void printfc(char c) {
+	if (out_pos == OBUF_SIZE) bflush();
+	outbuf[out_pos++] = c;
+}
+
+void printfi(int n) {
+	if (n == 0) { printfc('0'); return; }
+	if (n < 0) { printfc('-'); n = -n; }
+	char buf[12];
+	int idx = 0;
+	while (n) {
+		buf[idx++] = (n % 10) + '0';
+		n /= 10;
+	}
+	while (idx--) printfc(buf[idx]);
+}
+
 int n, m, ans = 0;
-char s[10001][501], c[501];
+char* ss[10001];
+char* cc;
 
 int com(const void* A, const void* B) {
-    return strcmp((char*)A, (char*)B);
+	const char* a = *(const char* const*)A;
+	const char* b = *(const char* const*)B;
+	return strcmp(a, b);
 }
 
 int isin() {
@@ -15,7 +95,7 @@ int isin() {
 
     while (l <= r) {
         mid = (l + r) / 2;
-        cm = strcmp(s[mid], c);
+        cm = strcmp(ss[mid], cc);
         if (cm == 0) return 1;
         else if (cm < 0) l = mid + 1;
         else r = mid - 1;
@@ -24,18 +104,19 @@ int isin() {
 }
 
 int main() {
-
-    scanf("%d %d", &n, &m);
+	init();
+	n = scanfi();
+	m = scanfi();
     for (int i = 0; i < n; i++) {
-        scanf("%s", s[i]);
+		ss[i] = scanfs();
     }
-    qsort(s, n, sizeof(s[0]), com);
+    qsort(ss, n, sizeof(ss[0]), com);
 
     for (int i = 0; i < m; i++) {
-        scanf("%s", c);
+		cc = scanfs();
         ans += isin();
     }
-    printf("%d\n", ans);
-
+	printfi(ans);
+	bflush();
     return 0;
 }
