f7e6cfa5be65ecc34c04859599e27232544a27a0
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-05-02 18:21:55 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 76 ms, Memory: 5032 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define min(a, b, c) (a < b ? (a < c ? a : c) : (b < c ? b : c))
 
 int n, m, map[1001][1001];
-int sum[1001][1001], s, mx = 0;
+int f[1001], s[1001], mx;
 
 int main() {
     scanf("%d %d", &n, &m);
@@ -10,20 +11,15 @@ int main() {
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             scanf("%1d", &map[i][j]);
-            sum[i][j] = map[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            for (int k = 1; k <= (i < j ? i : j); k++) {
-                s = sum[i][j] - sum[i - k][j] - sum[i][j - k] + sum[i - k][j - k];
-                if (s == k * k && s > mx) mx = s;
+            if (map[i][j] == 1) {
+                s[j] = min(s[j - 1], f[j], f[j - 1]) + 1;
+                mx = mx < s[j] ? s[j] : mx;
             }
+            else s[j] = 0;
         }
+        for(int j = 1; j <= m; j++) f[j] = s[j];
     }
-    printf("%d\n", mx);
-
-
+    printf("%d\n", mx*mx);
 
     return 0;
 }
