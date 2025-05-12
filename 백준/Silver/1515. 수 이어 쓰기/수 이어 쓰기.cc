1137bc5922dade2b1f0d95eaa78a07845be66078
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-05-12 20:18:54 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
     char S[3005];
     scanf("%s", S);
-    
+
     int len = strlen(S);
     int pos = 0;
-    for (int i = 1; ; i++){
+    for (int i = 1; ; i++) {
         char buf[12];
         int bl = 0;
         int t = i;
-        if (t == 0) buf[bl++] = '0';
-        else {
-            char tmp[12];
-            int tl = 0;
-            while (t > 0){
-                tmp[tl++] = '0' + (t % 10);
-                t /= 10;
-            }
-            while (tl > 0){
-                buf[bl++] = tmp[--tl];
-            }
+        char tmp[12];
+        int tl = 0;
+        while (t > 0) {
+            tmp[tl++] = '0' + (t % 10);
+            t /= 10;
         }
-        for (int j = 0; j < bl && pos < len; j++){
+        while (tl > 0) {
+            buf[bl++] = tmp[--tl];
+        }
+        for (int j = 0; j < bl && pos < len; j++) {
             if (buf[j] == S[pos]) pos++;
         }
-        if (pos == len){
+        if (pos == len) {
             printf("%d\n", i);
             break;
         }
