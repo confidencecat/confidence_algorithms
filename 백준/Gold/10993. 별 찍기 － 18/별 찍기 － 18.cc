d0a2a0c7090b0aa007b47fbc68c72aa4b650cc98
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-05-16 23:03:12 +0900

    [Gold IV] Title: 별 찍기 - 18, Time: 20 ms, Memory: 3168 KB -BaekjoonHub

--- 백준/Gold/10993. 별 찍기 － 18/별 찍기 － 18.cc
+++ 백준/Gold/10993. 별 찍기 － 18/별 찍기 － 18.cc
@@ -5,25 +5,20 @@
 int n;
 char s[1026][2048];
 
+int ww, hh, i, w, h;
+
 int main() {
-    //freopen("input.txt", "rt", stdin);
 
     scanf("%d", &n);
-    int w = (int)pow(2, (n + 1)) - 3;
-    int h = (int)pow(2, n) - 1;
+    for (i = 0, w = 1; i < (n + 1); i++, w *= 2); w -= 3;
+    for (i = 0, h = 1; i < (n); i++, h *= 2); h--;
     bool d = n%2==1;
     int sh = d ? 0 : h - 1;
 
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) s[i][j] = ' ';
-    }
-
-
-    int ww, hh;
     for (int m = n; m > 0; m--) {
-        ww = (int)pow(2, (m + 1)) - 3;
-        hh = (int)pow(2, m) - 1;
+        for (i = 0, ww = 1; i < (m + 1); i++, ww *= 2); ww -= 3;
+        for (i = 0, hh = 1; i < (m); i++, hh *= 2); hh--;
         sh = (!d ? sh - hh + 1 : sh);
         for (int i = 0; i <= ww / 2; i++) {
             s[i + sh][w / 2 + i + (d ? 0 : -ww / 2)] = '*';
@@ -35,7 +30,7 @@ int main() {
         d = !d;
     }
     for (int i = 0; i < h; i++) {
-        for (int j = 0; j < (n%2==1 ? w/2+i + 1 : w - i); j++) printf("%c", s[i][j]);
+        for (int j = 0; j < (n%2==1 ? w/2+i + 1 : w - i); j++) printf("%c", s[i][j]==NULL ? ' ' : s[i][j]);
         printf("\n");
     }
     
