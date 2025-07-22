e9b0761e599972925ee89dcbcf7175feebdfcf8e
confidencecat <89053337+confidencecat@users.noreply.github.com>
2025-07-22 10:07:11 +0900

    [Gold V] Title: 풍선 맞추기, Time: 88 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.cc
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.cc
@@ -2,26 +2,25 @@
 #include <stdio.h>
 
 int n, ans = 1;
-int a[1000001];
+int a;
 int dp[1000001];
 
 int main() {
 
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
-    dp[a[0] - 1] = 1;
+    scanf("%d", &a);
+    dp[a - 1] = 1;
     
     for (int i = 1; i < n; i++) {
-        if (dp[a[i]] >= 1) {
-            dp[a[i]]--;
-            if(a[i] != 1) dp[a[i] - 1]++;
+        scanf("%d", &a);
+        if (dp[a] >= 1) {
+            dp[a]--;
+            if(a != 1) dp[a - 1]++;
         }
         else {
             ans++;
-            dp[a[i] - 1]++;
+            dp[a - 1]++;
         }
         /*for (int j = 1; j <= 6; j++) {
             printf("%d ", dp[j]);
